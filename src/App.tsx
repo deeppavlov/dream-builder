b294494217d5262fb1dc3ad5ec5d182f3e9c14af
mablin7 <magyarb0201@gmail.com>
2021-06-24 08:37:57 +0300

    deps: add goober (css-in-js lib)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { setup } from "goober";
 
 import store from "./store";
 import EditorFrame from "./editor/EditorFrame";
 import Sidebar from "./sidebar/Sidebar";
+
+setup(React.createElement);
 
 function App() {
   return (

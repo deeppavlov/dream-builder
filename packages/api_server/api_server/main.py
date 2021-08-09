8ecef89d476d63c08638dea5e0c83d35d35b4c0e
mablin7 <magyarb0201@gmail.com>
2021-08-09 14:26:45 +0200

    feat: wip new API

--- packages/api_server/api_server/main.py
+++ packages/api_server/api_server/main.py
@@ -1,40 +1,69 @@
-from typing import Optional, List
+from typing import List, Dict, Any, Optional
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from fastapi import FastAPI, Depends
-from nanoid import generate
 
-from .database import connect_db, close_db, get_db
-from .res import Resource, get_res, get_all_with_type, insert_res, hooks_on_startup, hooks_on_shutdown
+from .resdb.mongodb import DBResChild, init_db, close_db, get_db, get_resource, get_resources_with_type, get_children_with_type, insert_resource
+from api_types_py import Component, Model
 
 app = FastAPI()
-
-app.add_event_handler("startup", connect_db)
-app.add_event_handler("startup", hooks_on_startup)
+app.add_event_handler("startup", init_db)
 app.add_event_handler("shutdown", close_db)
-app.add_event_handler("shutdown", hooks_on_shutdown)
 
-@app.get("/api/res/{resid}", response_model=Resource)
-async def getres(resid: str, db: AsyncIOMotorDatabase = Depends(get_db)):
-    return await get_res(db, resid)
+@app.get("/components", response_model=List[Component])
+async def list_comps(type: Optional[str] = None, db: AsyncIOMotorDatabase = Depends(get_db)):
+    resources = await get_resources_with_type(db, type="component")
+    return [ res.content for res in resources if type is None or res.content['type'] == type ]
 
-@app.put("/api/res/{resid}", response_model=Resource)
-async def putres(resid: str, res: Resource, disable_hooks: int = 0, db: AsyncIOMotorDatabase = Depends(get_db)):
-    res.resid = resid
-    print("disable hooks", disable_hooks)
-    await insert_res(db, res, disable_hooks=disable_hooks == 1)
-    return await get_res(db, resid)
+@app.post("/components")
+async def post_comp(comp: Component, db: AsyncIOMotorDatabase = Depends(get_db)):
+    newres = await insert_resource(db, type="component", content=comp)
+    return newres.resid
 
-@app.post("/api/res", response_model=Resource)
-async def postres(res: Resource, disable_hooks: int = 0, db: AsyncIOMotorDatabase = Depends(get_db)):
-    resid = generate()
-    res.resid = resid
-    await insert_res(db, res, disable_hooks=disable_hooks == 1)
-    return await get_res(db, resid)
+@app.get("/components/{resid}", response_model=Component)
+async def get_comp(resid: str, db: AsyncIOMotorDatabase = Depends(get_db)):
+    res = await get_resource(db, resid)
+    return res.content
 
-@app.get("/api/res", response_model=List[Resource])
-async def findres(type: Optional[str] = None, db: AsyncIOMotorDatabase = Depends(get_db)):
-    if type is not None:
-        return await get_all_with_type(db, type)
-    else:
-        return []
+# @app.patch("/components/{resid}")
+# async def patch_comp(resid: str, db: AsyncIOMotorDatabase = Depends(get_db)):
+#     pass
+
+# @app.delete("/components/{resid}")
+# async def delete_comp(resid: str, db: AsyncIOMotorDatabase = Depends(get_db)):
+#     pass
+
+@app.get("/components/{resid}/models", response_model=List[Model])
+async def list_models(resid: str, db: AsyncIOMotorDatabase = Depends(get_db)):
+    resources = await get_children_with_type(db, parentid=resid, children_type="model")
+    return [ res.content for res in resources ]
+
+@app.post("/components/{resid}/models")
+async def post_model(resid: str, db: AsyncIOMotorDatabase = Depends(get_db)):
+    modelid = (await insert_resource(db, type="model", content=Model())).resid
+    comp = await get_resource(db, resid)
+    await insert_resource(db, resid=resid, children=[*comp.children, DBResChild(resid=modelid, hash="")])
+    return modelid
+
+@app.get("/components/{resid}/{data_type_plural}")
+async def list_data(resid: str, data_type_plural: str, db: AsyncIOMotorDatabase = Depends(get_db)):
+    data_type = data_type_plural.rstrip("s")
+    resources = await get_children_with_type(db, parentid=resid, children_type=data_type)
+    return [ res.content for res in resources ]
+
+@app.post("/components/{resid}/{data_type_plural}")
+async def post_data(resid: str, data_type_plural: str, data: Dict[str, Any], db: AsyncIOMotorDatabase = Depends(get_db)):
+    data_type = data_type_plural.rstrip("s")
+    dataid = (await insert_resource(db, type=data_type, content=data)).resid
+    comp = await get_resource(db, resid)
+    await insert_resource(db, resid=resid, children=[*comp.children, DBResChild(resid=dataid, hash="")])
+    return dataid
+
+@app.put("/components/{resid}/{data_type_plural}/{dataid}")
+async def put_data(dataid: str, newData: Dict[str, Any], db: AsyncIOMotorDatabase = Depends(get_db)):
+    await insert_resource(db, resid=dataid, content=newData)
+
+
+# @app.delete("/components/{resid}/{data_type}/{dataid}")
+# async def delete_data(resid: str, data_type: str, dataid: str, db: AsyncIOMotorDatabase = Depends(get_db)):
+#     return f"delete data with type {data_type} and id {dataid} in component id {resid}"
 
---
+++ packages/api_server/api_server/resdb/mongodb.py
@@ -0,0 +1,83 @@
+import os, json
+from time import time
+from typing import List, Dict, Any, Optional
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from nanoid import generate
+from hashlib import sha1
+
+class DBResChild(BaseModel):
+    resid: str
+    hash: str
+
+class DBResource(BaseModel):
+    resid: str
+    type: str
+    hash: str
+    latest: bool
+    timestamp: int
+    children: List[DBResChild]
+    content: Dict[str, Any]
+
+DB_URL = os.environ["MONGODB_URL"]
+db_client: AsyncIOMotorClient = None
+
+def get_db() -> AsyncIOMotorDatabase:
+    global db_client
+    return db_client['dp-builder']
+
+async def init_db():
+    global db_client
+    db_client = AsyncIOMotorClient(DB_URL)
+
+async def close_db():
+    global db_client
+    db_client.close()
+
+async def get_resource(db: AsyncIOMotorDatabase, res_id: str) -> DBResource:
+    res = await db.resources.find_one({ 'resid': res_id, 'latest': True })
+    return DBResource(**res)
+
+async def get_resources_with_type(db: AsyncIOMotorDatabase, type: str) -> List[DBResource]:
+    res = db.resources.find({ 'type': type, 'latest': True })
+    return [DBResource(**r) for r in await res.to_list(None)]
+
+async def get_children_with_type(db: AsyncIOMotorDatabase, parentid: str, children_type: str) -> List[DBResource]:
+    parent = await get_resource(db, parentid)
+    children_ids = [ c.resid for c in parent.children ]
+    res = db.resources.find({ 'type': children_type, 'resid': { '$in': children_ids }, 'latest': True })
+    return [DBResource(**r) for r in await res.to_list(None)]
+
+async def insert_resource(db: AsyncIOMotorDatabase, *, resid: Optional[str] = None, type: Optional[str] = None, content: Optional[Any] = None, children: Optional[List[DBResChild]] = None) -> DBResource:
+    if resid is None:
+        resid = generate()
+        children = children or []
+        if type is None:
+            raise ValueError("Type is required for a new resource")
+        if content is None:
+            raise ValueError("Content is required for a new resource")
+    else:
+        if content is None and children is None:
+            raise ValueError("Either content or children is required")
+        old_res = await get_resource(db, resid)
+        type = old_res.type
+        children = children or old_res.children
+        content = content or old_res.content  # type: ignore
+        await db.resources.update_many({ 'resid': resid }, { '$set': { 'latest': False } })
+
+    content_dict = jsonable_encoder(content)
+    revhash = sha1()
+    revhash.update(json.dumps(content_dict, sort_keys=True, ensure_ascii=True).encode())
+    newrev = DBResource(
+        resid=resid,
+        type=type,
+        latest=True,
+        hash=revhash.hexdigest(),
+        timestamp=int(time()),
+        content=content_dict,
+        children=children
+    )
+    await db.resources.insert_one(jsonable_encoder(newrev))
+    return newrev
+

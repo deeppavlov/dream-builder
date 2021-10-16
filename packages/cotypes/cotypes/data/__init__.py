6b0c61312a291d93b2114687976907c103f42449
mablin7 <magyarb0201@gmail.com>
2021-10-16 13:00:20 +0300

    fix: gitignore

---
+++ packages/cotypes/cotypes/data/__init__.py
@@ -0,0 +1,4 @@
+from .flow import Flow
+from .slot import Slot
+from .intent import Intent
+__all__ = ['flow', 'slot', 'intent']
---
+++ packages/cotypes/ts/data/index.d.ts
@@ -0,0 +1,7 @@
+import { Flow } from './flow';
+import { Slot } from './slot';
+import { Intent } from './intent';
+export { Flow } from './flow';
+export { Slot } from './slot';
+export { Intent } from './intent';
+export type Data = Flow | Slot | Intent;

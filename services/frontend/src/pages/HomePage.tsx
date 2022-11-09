56a42313e7b1cbdb7e37ced2cfdb9450ad93c248
Artem Klementev <o.klmntv@gmail.com>
2022-11-09 11:52:43 +0300

    new screen for landing

--- services/frontend/src/pages/HomePage.tsx
+++ services/frontend/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
+import { useTooltip } from '../hooks/useTooltip'
 import { Topbar } from '../components/Topbar/Topbar'
 import { Main } from '../components/Main/Main'
 import { Home } from '../components/Home/Home'
 export const HomePage = () => {
+  useTooltip()
   return (
     <>
-      <Topbar type='welcome' />
+      <Topbar type='home' />
       <Main sidebar='none'>
         <Home />
       </Main>

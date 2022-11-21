ac798c4d95d93d37cd41c841172cd10d112e984a
Artem Klementev <o.klmntv@gmail.com>
2022-11-21 16:21:24 +0300

    new components in sidebar&shadow

--- services/frontend/src/components/Sidebar/Sidebar.tsx
+++ services/frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,20 +1,14 @@
-import { CPU } from './components/CPU'
-import { Puzzle } from './components/Puzzle'
 import { Settings } from './components/Settings'
 import s from './Sidebar.module.scss'
 
-export const Sidebar = ({ children, sidebar, type }: any) => {
+export const Sidebar = ({ children, sidebar, type, buttons }: any) => {
   switch (type) {
     case 'editor':
       return (
         <>
           <div className={s.sidebar}>
-            <div className={s.btns_area}>
-              <CPU />
-              <Puzzle />
-            </div>
+            <div className={s.btns_area}>{children}</div>
             <Settings />
-            {children}
           </div>
         </>
       )

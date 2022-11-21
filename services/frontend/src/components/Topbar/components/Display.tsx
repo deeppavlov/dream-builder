da95d16d2313253856499db3aac1b98ebc291f32
Artem Klementev <o.klmntv@gmail.com>
2022-11-21 16:32:18 +0300

    view changer

--- services/frontend/src/components/Topbar/components/Display.tsx
+++ services/frontend/src/components/Topbar/components/Display.tsx
@@ -3,13 +3,18 @@ import { ReactComponent as CardsView } from '../../../assets/icons/display.svg'
 import { ReactComponent as ListView } from '../../../assets/icons/list-view.svg'
 import s from './Display.module.scss'
 
-export const Display = () => {
+export const Display = ({ viewHandler }: any) => {
   const [view, setView] = useState(false)
   const changeView = () => {
     setView(!view)
+    viewHandler()
   }
   return (
-    <button onClick={changeView} className={s.display}>
+    <button
+      data-tip='Change View Type'
+      data-for='topbar_tooltip'
+      onClick={changeView}
+      className={s.display}>
       {view ? <CardsView /> : <ListView />}
     </button>
   )

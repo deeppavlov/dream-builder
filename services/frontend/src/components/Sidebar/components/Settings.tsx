0ac6020041d22c86d43fe988cb7ed3553d9fe3fd
Artem Klementev <o.klmntv@gmail.com>
2022-11-21 16:31:43 +0300

    settings component for sidebar

--- services/frontend/src/components/Sidebar/components/Settings.tsx
+++ services/frontend/src/components/Sidebar/components/Settings.tsx
@@ -1,5 +1,9 @@
 import s from './Settings.module.scss'
-
+import { ReactComponent as Gear } from '../../../assets/icons/gear.svg'
 export const Settings = () => {
-  return <button className={s.settings} />
+  return (
+    <button className={s.settings}>
+      <Gear />
+    </button>
+  )
 }

96b0e564f900b6020b35be925aa3bc3130e3a489
Artem Klementev <o.klmntv@gmail.com>
2022-11-21 16:32:36 +0300

    small fix

--- services/frontend/src/components/Topbar/components/Menu.tsx
+++ services/frontend/src/components/Topbar/components/Menu.tsx
@@ -1,10 +1,12 @@
+import { BurgerMenu } from '../../BurgerMenu/BurgerMenu'
 import s from './Menu.module.scss'
 
 export const TopbarMenu = () => {
   return (
-    <div className={s.menu}>
-      <span className={s.brgr_menu}></span>
-      <span className={s.brgr_menu}></span>
-    </div>
+    <button data-tip data-for='burgermenu' className={s.menu}>
+      <span className={s.brgr_menu} />
+      <span className={s.brgr_menu} />
+      <BurgerMenu />
+    </button>
   )
 }

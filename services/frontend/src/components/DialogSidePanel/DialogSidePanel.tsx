f41463a14087294bd1b964ad12cefac186bd56c5
Timofey Syrov <warfacetopic@mail.ru>
2022-11-29 18:42:19 +0300

    added DialogButton component and dialog chatHistory styles

--- services/frontend/src/components/DialogSidePanel/DialogSidePanel.tsx
+++ services/frontend/src/components/DialogSidePanel/DialogSidePanel.tsx
@@ -1,6 +1,8 @@
+import React, { useRef, useState } from 'react'
 import Modal from 'react-modal'
 import { ReactComponent as CloseIcon } from '@assets/icons/close.svg'
-import DialogTextIcon from '@assets/icons/dialogText.svg'
+import { ReactComponent as DialogTextIcon } from '@assets/icons/dialog_text.svg'
+import DialogButton from '../DialogButton/DialogButton'
 import s from './DialogSidePanel.module.scss'
 
 const customStyles = {
@@ -19,13 +21,42 @@ const customStyles = {
   },
 }
 
-interface DialogSidePanel {
+const TEXT_CHAT_TYPE = 'text'
+const VOICE_CHAT_TYPE = 'voice'
+
+type ChatType = typeof TEXT_CHAT_TYPE | typeof VOICE_CHAT_TYPE
+interface ChatMessage {
+  byBot: boolean
+  text: string
+}
+type props = {
   isOpen: boolean
   setIsOpen: Function
 }
 
-const DialogSidePanel = ({ isOpen, setIsOpen }: DialogSidePanel) => {
+const DialogSidePanel = ({ isOpen, setIsOpen }: props) => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
+  const [chatType, setChatType] = useState<ChatType>(TEXT_CHAT_TYPE)
+  const [chatHistory, setChatHistory] = useState<ChatMessage[] | []>([
+    { byBot: false, text: 'Hello!' },
+    { byBot: true, text: 'Hi! This is a Dream Socialbot.   How are you?' },
+  ])
+  const [isError, setIsError] = useState(false)
+
+  const isTextChat = chatType === TEXT_CHAT_TYPE
+  const isVoiceChat = chatType === VOICE_CHAT_TYPE
+  const isFirstTest = chatHistory === null
+
   const closeModal = () => setIsOpen(false)
+  const handleChatTypeBtn = (type: ChatType) => setChatType(type)
+  const handleSubmit = (e: React.MouseEvent) => {
+    const userMessage = textAreaRef.current?.value
+    if (!textAreaRef.current) return
+    if (!userMessage && userMessage === '') return
+
+    setChatHistory([...chatHistory, ...[{ byBot: false, text: userMessage! }]])
+    textAreaRef.current.value = ''
+  }
 
   return (
     <Modal
@@ -41,22 +72,47 @@ const DialogSidePanel = ({ isOpen, setIsOpen }: DialogSidePanel) => {
             onClick={closeModal}
           />
         </div>
-        <div className={s['dialogSidePanel__chat-box']}></div>
+        <div className={s['dialogSidePanel__chat-box']}>
+          <div className={s.chat}>
+            {chatHistory?.map((m, index) => (
+              <div
+                key={`${m.byBot}${index}`}
+                className={`${s.chat__container} ${
+                  m.byBot && s.chat__container_bot
+                }`}>
+                {m.byBot && <span className={s.chat__avatar}></span>}
+                <span
+                  className={`${s.chat__message} ${
+                    m.byBot && s.chat__message_bot
+                  }`}>
+                  {m.text}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
         <div className={s.dialogSidePanel__controls}>
-          <img
-            src={DialogTextIcon}
-            alt='Text'
-            className={s['dialogSidePanel__control-btn']}
-          />
+          <DialogButton
+            active={isTextChat}
+            onClick={() => handleChatTypeBtn(TEXT_CHAT_TYPE)}>
+            <DialogTextIcon />
+          </DialogButton>
+          {/* <DialogButton
+            active={isVoiceChat}
+            onClick={() => handleChatTypeBtn(VOICE_CHAT_TYPE)}>
+            <DialogMicrophoneIcon />
+          </DialogButton> */}
         </div>
         <textarea
+          ref={textAreaRef}
           className={s.dialogSidePanel__textarea}
           name='dialog'
           id='dialog'
           placeholder='Type...'
         />
         <div className={s.dialogSidePanel__btns}>
-          <button>Send</button>
+          {/* Change to onSubmit, maybe need to wrap TextArea and button in to HTML Form */}
+          <button onClick={handleSubmit}>Send</button>
         </div>
       </div>
     </Modal>
--- services/frontend/src/components/Topbar/components/Test.tsx
+++ services/frontend/src/components/Topbar/components/Test.tsx
@@ -4,11 +4,16 @@ import DialogSidePanel from '../../DialogSidePanel/DialogSidePanel'
 import s from './Test.module.scss'
 
 export const Test = () => {
-  const [modalIsOpen, setIsOpen] = useState(true)
+  const [modalIsOpen, setIsOpen] = useState(false)
 
   return (
     <button data-tip='Chat With Your Bot' className={s.test}>
-      <img src={Play} alt='Play' className={s.test} onClick={() => setIsOpen(true)} />
+      <img
+        src={Play}
+        alt='Play'
+        className={s.test}
+        onClick={() => setIsOpen(true)}
+      />
       <DialogSidePanel isOpen={modalIsOpen} setIsOpen={setIsOpen} />
     </button>
   )

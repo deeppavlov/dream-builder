d8e54445e82ea8b81bf82ad2e523fd7a180ac034
Artem Klementev <o.klmntv@gmail.com>
2022-12-01 02:04:00 +0300

    annotators with new components + refactoring

--- services/frontend/src/components/Annotators/Element.tsx
+++ services/frontend/src/components/Annotators/Element.tsx
@@ -1,3 +1,4 @@
+import SkillLogo from '../../assets/icons/skillIcon.svg'
 import { KebabButton } from '../../ui/KebabButton/KebabButton'
 import { ToggleButton } from '../../ui/ToggleButton/ToggleButton'
 import s from './Element.module.scss'
@@ -7,11 +8,11 @@ export const Element = ({}) => {
     <div className={s.element}>
       <div className={s.left}>
         <div className={s.top}>
-          <span className={s.icon}></span>
-          <h6>Fact Retrieval</h6>
+          <img className={s.icon} src={SkillLogo} />
+          <p className={s.name}>Fact Retrieval</p>
         </div>
         <div className={s.bottom}>
-          <p className={s.data}>1.5 Gb RAM | 0.0 Gb GPU</p>
+          <p className={s.data}>RAM:1.5Gb | GPU:0.0Gb </p>
         </div>
       </div>
       <div className={s.right}>

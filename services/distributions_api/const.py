d0a67a9e60e9fc00ab81e108bd14c09f91b005da
ilyazat <ilyazovazat@gmail.com>
2022-12-01 04:49:16 +0300

    minor fix dockerfile

--- services/distributions_api/const.py
+++ services/distributions_api/const.py
@@ -16,8 +16,7 @@ from deeppavlov_dreamtools.distconfigs.generics import (
 )
 
 
-# DREAM_ROOT_PATH = Path(os.environ["DREAM_ROOT_PATH"])
-DREAM_ROOT_PATH = Path(__file__).parents[2] / "dream"
+DREAM_ROOT_PATH = Path(os.environ["DREAM_ROOT_PATH"])
 
 ASSISTANT_DISTS_PATH = DREAM_ROOT_PATH / "assistant_dists"
 

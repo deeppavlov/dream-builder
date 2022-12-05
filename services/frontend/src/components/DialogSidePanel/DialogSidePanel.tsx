3129f6120f431c9e2e8bfef08ce65bcc4829a67c
Timofey Syrov <warfacetopic@mail.ru>
2022-12-05 12:30:50 +0300

    added start & error states for DialogSidePanel

--- services/frontend/src/components/DialogSidePanel/DialogSidePanel.tsx
+++ services/frontend/src/components/DialogSidePanel/DialogSidePanel.tsx
@@ -1,25 +1,8 @@
 import React, { useRef, useState } from 'react'
-import Modal from 'react-modal'
-import { ReactComponent as CloseIcon } from '@assets/icons/close.svg'
 import { ReactComponent as DialogTextIcon } from '@assets/icons/dialog_text.svg'
 import DialogButton from '../DialogButton/DialogButton'
+import BaseSidePanel from '../BaseSidePanel/BaseSidePanel'
 import s from './DialogSidePanel.module.scss'
-
-const customStyles = {
-  overlay: {
-    background: 'transparent',
-  },
-  content: {
-    top: 64,
-    left: 'fit-with',
-    right: '0',
-    bottom: '0',
-    border: 'none',
-    background: 'transparent',
-    borderRadius: 'none',
-    padding: 'none',
-  },
-}
 
 const TEXT_CHAT_TYPE = 'text'
 const VOICE_CHAT_TYPE = 'voice'
@@ -29,25 +12,37 @@ interface ChatMessage {
   byBot: boolean
   text: string
 }
-type props = {
+interface props {
+  error?: boolean
+  start?: boolean
   isOpen: boolean
-  setIsOpen: Function
+  setIsOpen: (state: boolean) => void
+  positions?: Partial<{
+    top: number
+    left: number
+    right: number
+    bottom: number
+  }>
 }
 
-const DialogSidePanel = ({ isOpen, setIsOpen }: props) => {
+const DialogSidePanel = ({
+  isOpen,
+  setIsOpen,
+  positions,
+  error,
+  start,
+}: props) => {
   const textAreaRef = useRef<HTMLTextAreaElement>(null)
   const [chatType, setChatType] = useState<ChatType>(TEXT_CHAT_TYPE)
   const [chatHistory, setChatHistory] = useState<ChatMessage[] | []>([
     { byBot: false, text: 'Hello!' },
     { byBot: true, text: 'Hi! This is a Dream Socialbot.   How are you?' },
   ])
-  const [isError, setIsError] = useState(false)
-
+  const [isError, setIsError] = useState(error ?? false)
+  const [isFirstTest, setIsFirstTest] = useState(start ?? chatHistory === null)
   const isTextChat = chatType === TEXT_CHAT_TYPE
   const isVoiceChat = chatType === VOICE_CHAT_TYPE
-  const isFirstTest = chatHistory === null
 
-  const closeModal = () => setIsOpen(false)
   const handleChatTypeBtn = (type: ChatType) => setChatType(type)
   const handleSubmit = (e: React.MouseEvent) => {
     const userMessage = textAreaRef.current?.value
@@ -57,65 +52,83 @@ const DialogSidePanel = ({ isOpen, setIsOpen }: props) => {
     setChatHistory([...chatHistory, ...[{ byBot: false, text: userMessage! }]])
     textAreaRef.current.value = ''
   }
+  const handleStartBtnClick = () => setIsFirstTest(false)
+  const handleGoBackBtnClick = () => setIsOpen(false)
 
   return (
-    <Modal
+    <BaseSidePanel
       isOpen={isOpen}
-      onRequestClose={closeModal}
-      style={customStyles}
-      contentLabel='Test Dialog'>
-      <div className={s.dialogSidePanel}>
-        <div className={s.dialogSidePanel__title}>
-          <span>Dialog</span>
-          <CloseIcon
-            className={s.dialogSidePanel__close}
-            onClick={closeModal}
-          />
-        </div>
-        <div className={s['dialogSidePanel__chat-box']}>
-          <div className={s.chat}>
-            {chatHistory?.map((m, index) => (
-              <div
-                key={`${m.byBot}${index}`}
-                className={`${s.chat__container} ${
-                  m.byBot && s.chat__container_bot
-                }`}>
-                {m.byBot && <span className={s.chat__avatar}></span>}
-                <span
-                  className={`${s.chat__message} ${
-                    m.byBot && s.chat__message_bot
-                  }`}>
-                  {m.text}
-                </span>
+      setIsOpen={setIsOpen}
+      position={positions}
+      name='Dialog'>
+      <div
+        className={`${s.dialogSidePanel} ${
+          isFirstTest && !isError && s.dialogSidePanel_start
+        } ${isError && s.dialogSidePanel_error}`}>
+        {isError && (
+          <>
+            <span className={s['dialogSidePanel__alert-name']}>Alert!</span>
+            <p className={s['dialogSidePanel__alert-desc']}>
+              Check the settings of your bot. Something went wrong!
+            </p>
+            <button onClick={handleGoBackBtnClick}>Go back</button>
+          </>
+        )}
+        {isFirstTest && !isError && (
+          <>
+            <span className={s['dialogSidePanel__alert-name']}>
+              Run your bot
+            </span>
+            <p className={s['dialogSidePanel__alert-desc']}>
+              Start a test to interact with your bot using text, voice or
+              buttons
+            </p>
+            <button onClick={handleStartBtnClick}>Run Test</button>
+          </>
+        )}
+        {!isFirstTest && !isError && (
+          <>
+            <div className={s['dialogSidePanel__chat']}>
+              <div className={s.chat}>
+                {chatHistory?.map((m, index) => (
+                  <div
+                    key={`${m.byBot}${index}`}
+                    className={`${s.chat__container} ${
+                      m.byBot && s.chat__container_bot
+                    }`}>
+                    {m.byBot && <span className={s.chat__avatar}></span>}
+                    <span
+                      className={`${s.chat__message} ${
+                        m.byBot && s.chat__message_bot
+                      }`}>
+                      {m.text}
+                    </span>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </div>
-        <div className={s.dialogSidePanel__controls}>
-          <DialogButton
-            active={isTextChat}
-            onClick={() => handleChatTypeBtn(TEXT_CHAT_TYPE)}>
-            <DialogTextIcon />
-          </DialogButton>
-          {/* <DialogButton
-            active={isVoiceChat}
-            onClick={() => handleChatTypeBtn(VOICE_CHAT_TYPE)}>
-            <DialogMicrophoneIcon />
-          </DialogButton> */}
-        </div>
-        <textarea
-          ref={textAreaRef}
-          className={s.dialogSidePanel__textarea}
-          name='dialog'
-          id='dialog'
-          placeholder='Type...'
-        />
-        <div className={s.dialogSidePanel__btns}>
-          {/* Change to onSubmit, maybe need to wrap TextArea and button in to HTML Form */}
-          <button onClick={handleSubmit}>Send</button>
-        </div>
+            </div>
+            <div className={s.dialogSidePanel__controls}>
+              <DialogButton
+                active={isTextChat}
+                onClick={() => handleChatTypeBtn(TEXT_CHAT_TYPE)}>
+                <DialogTextIcon />
+              </DialogButton>
+            </div>
+            <textarea
+              ref={textAreaRef}
+              className={s.dialogSidePanel__textarea}
+              name='dialog'
+              id='dialog'
+              placeholder='Type...'
+            />
+            <div className={s.dialogSidePanel__btns}>
+              {/* Change to onSubmit, maybe need to wrap TextArea and button in to HTML Form */}
+              <button onClick={handleSubmit}>Send</button>
+            </div>
+          </>
+        )}
       </div>
-    </Modal>
+    </BaseSidePanel>
   )
 }
 

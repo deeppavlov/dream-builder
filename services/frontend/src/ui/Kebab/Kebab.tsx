84e9b1f36b267b310fd7e09c2b9cd3cc3a53bb92
Artem Klementev <o.klmntv@gmail.com>
2022-12-08 15:22:25 +0300

    kebab buttons fix

---
+++ services/frontend/src/ui/Kebab/Kebab.tsx
@@ -0,0 +1,19 @@
+import { MenuList } from '../../components/MenuList/MenuList'
+import s from './Kebab.module.scss'
+
+export const Kebab = ({ disabled, type, color, dataFor }: any) => {
+  console.log(disabled)
+  return (
+    <button
+      disabled={disabled === undefined ? false : disabled}
+      data-tip
+      data-for={dataFor}
+      style={{ flexDirection: `${type}` }}
+      className={s.kebab}>
+      <figure style={{ backgroundColor: `${color}` }} className={s.dots} />
+      <figure style={{ backgroundColor: `${color}` }} className={s.dots} />
+      <figure style={{ backgroundColor: `${color}` }} className={s.dots} />
+      <MenuList type={dataFor} />
+    </button>
+  )
+}
--- services/frontend/src/ui/KebabButton/KebabButton.tsx
+++ services/frontend/src/ui/KebabButton/KebabButton.tsx
@@ -1,21 +1,12 @@
-import { useTooltip } from '../../hooks/useTooltip'
-import Button from '../Button/Button'
+import { ReactComponent as KebabLogoSVG } from '../../assets/icons/kebab.svg'
+import { MenuList } from '../../components/MenuList/MenuList'
 import s from './KebabButton.module.scss'
 
-export const KebabButton = ({ type, color, wrapper}: any) => {
-  return wrapper ? (
-    <Button theme='secondary' small={true}>
-      <div style={{ flexDirection: `${type}` }} className={s.kebab}>
-        <figure className={s.dots} />
-        <figure className={s.dots} />
-        <figure className={s.dots} />
-      </div>
-    </Button>
-  ) : (
-    <button style={{ flexDirection: `${type}` }} className={s.kebab}>
-      <figure style={{ backgroundColor: `${color}` }} className={s.dots} />
-      <figure style={{ backgroundColor: `${color}` }} className={s.dots} />
-      <figure style={{ backgroundColor: `${color}` }} className={s.dots} />
+export const KebabButton = ({ dataFor }: any) => {
+  return (
+    <button data-tip data-for={dataFor} className={s.kebab}>
+      <KebabLogoSVG />
+      <MenuList type={dataFor} />
     </button>
   )
 }

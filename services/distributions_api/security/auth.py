c69da98a14938b84beecccd722af9e93f71758bf
ilyazat <ilyazovazat@gmail.com>
2022-12-12 20:21:05 +0300

    fix service interaction

--- services/distributions_api/security/auth.py
+++ services/distributions_api/security/auth.py
@@ -4,9 +4,9 @@ from fastapi import Header, HTTPException
 
 
 async def verify_token(jwt_data: str = Header()):
-    header = {"jwt": jwt_data}
+    header = {"jwt-data": jwt_data}
 
     async with aiohttp.ClientSession(headers=header) as session:
         async with session.get(AUTH_URL) as response:
-            if response != 200:
+            if response.status != 200:
                 raise HTTPException(status_code=400, detail="bad token")

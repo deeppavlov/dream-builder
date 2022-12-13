91444541f15747cc3632cab26c81337e8e24e430
ilyazat <ilyazovazat@gmail.com>
2022-12-13 13:07:52 +0300

    fix reading auth_url

--- services/distributions_api/security/auth.py
+++ services/distributions_api/security/auth.py
@@ -1,11 +1,10 @@
 import aiohttp
-from config import AUTH_URL
+from const import AUTH_URL
 from fastapi import Header, HTTPException
 
 
 async def verify_token(jwt_data: str = Header()):
     header = {"jwt-data": jwt_data}
-
     async with aiohttp.ClientSession(headers=header) as session:
         async with session.get(AUTH_URL) as response:
             if response.status != 200:

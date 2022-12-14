a0a63d4bbd95c3d1f17723fabb1f7133bbe61222
mtalimanchuk <mtalimanchuk@gmail.com>
2022-12-14 20:00:18 +0400

    fix cors issue

--- services/distributions_api/app.py
+++ services/distributions_api/app.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from services.distributions_api.routes.configs import router as configs_router
 from services.distributions_api.routes.distribution import router as distribution_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(distribution_router)
 app.include_router(configs_router)

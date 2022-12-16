05020be8fc100928ab4598dcbbe9c12396457706
Timofey Syrov <warfacetopic@mail.ru>
2022-12-16 17:15:23 +0300

    added AuthProvider wrapper for app

--- services/frontend/src/main.tsx
+++ services/frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './services/AuthProvider'
 import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 )

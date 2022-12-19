3def07692c15b22ce327a83619f623fe9edb7b74
Timofey Syrov <warfacetopic@mail.ru>
2022-12-19 13:44:18 +0300

    added disable state for IntentCatcherSidePanel

--- services/frontend/src/components/IntentCatcherSidePanel/IntentCatcherSidePanel.tsx
+++ services/frontend/src/components/IntentCatcherSidePanel/IntentCatcherSidePanel.tsx
@@ -4,95 +4,115 @@ import { SidePanelProps } from '../../ui/SidePanel/SidePanel'
 import Button from '../../ui/Button/Button'
 import BaseSidePanel from '../BaseSidePanel/BaseSidePanel'
 import IntentListItem, {
-  IntentListItemProps,
+  IntentListItemInterface,
 } from '../IntentListItem/IntentListItem'
 import IntentList from '../IntentList/IntentList'
 import IntentCatcherModal from '../IntentCatcherModal/IntentCatcherModal'
 import BaseLink from '../BaseLink/BaseLink'
 import s from './IntentCatcherSidePanel.module.scss'
+import SidePanelStatus from '../SidePanelStatus/SidePanelStatus'
+import { nanoid } from 'nanoid'
+import ReactTooltip from 'react-tooltip'
 
-const intentsMock: IntentListItemProps[] = [
+const intentsMock: IntentListItemInterface[] = [
   {
+    id: nanoid(8),
     name: 'Want_pizza',
     about: 'want pizza, wanna pizza, love pizza, like pizza...',
     status: 'error',
   },
   {
+    id: nanoid(8),
     name: 'Hello',
     about: 'Hey, Hello, Hi',
     status: 'error',
   },
   {
+    id: nanoid(8),
     name: 'Yes',
     about: 'yes, yeah, alright, ok',
     status: 'error',
   },
   {
+    id: nanoid(8),
     name: 'No',
     about: 'No, Nope, Do not do it',
     status: 'error',
   },
   {
+    id: nanoid(8),
     name: 'Help',
     about: 'sos, help, help me',
     status: 'error',
   },
   {
+    id: nanoid(8),
     name: 'Stop',
     about: 'stop, stop it, let it stop',
     status: 'warning',
   },
   {
+    id: nanoid(8),
     name: 'Cancel',
     about: 'Cancel, cancel it',
     status: 'warning',
   },
   {
+    id: nanoid(8),
     name: 'Fallback',
     about: 'Fallback',
     status: 'success',
   },
   {
+    id: nanoid(8),
     name: 'Bye',
     about: 'Bye, See you, Goodbye, See ya',
     status: 'success',
   },
   {
+    id: nanoid(8),
     name: 'Stop',
     about: 'stop, stop it, let it stop',
     status: 'warning',
   },
   {
+    id: nanoid(8),
     name: 'Cancel',
     about: 'Cancel, cancel it',
     status: 'warning',
   },
   {
+    id: nanoid(8),
     name: 'Fallback',
     about: 'Fallback',
     status: 'success',
   },
   {
+    id: nanoid(8),
     name: 'Bye',
     about: 'Bye, See you, Goodbye, See ya',
     status: 'success',
   },
   {
+    id: nanoid(8),
     name: 'Stop',
     about: 'stop, stop it, let it stop',
     status: 'warning',
   },
   {
+    id: nanoid(8),
     name: 'Cancel',
     about: 'Cancel, cancel it',
     status: 'warning',
   },
   {
+    id: nanoid(8),
     name: 'Fallback',
     about: 'Fallback',
     status: 'success',
   },
   {
+    id: nanoid(8),
     name: 'Bye',
     about: 'Bye, See you, Goodbye, See ya',
     status: 'success',
@@ -103,32 +123,43 @@ const IntentCatcherSidePanel = ({
   isOpen,
   setIsOpen,
   position,
+  disabled,
 }: SidePanelProps) => {
   /* `isTrainingLoading - training loading status` */
   /* `isTraining` - status from start training to Replace IntentCatcher*/
   const [isTrainingLoading, setIsTrainingLoading] = useState(false)
   const [isTraining, setIsTraining] = useState(false)
+  const [isDisabled, setIsDisabled] = useState(disabled ?? false)
   const [trainingResponse, setTrainingResponse] = useState<{
-    status: 'success' | 'failed'
+    status: 'success' | 'error'
   } | null>(null)
   const [addModalIsOpen, setAddModalIsOpen] = useState(false)
+  const [isNotified, setIsNotified] = useState(false)
+
   const handleAddIntentBtnClick = () => setAddModalIsOpen(true)
+
   const handleTrainBtnClick = () => {
     setIsTraining(true)
     setIsTrainingLoading(true)
     // Mock timeout
     setTimeout(() => {
       setTrainingResponse({
-        status: ['success', 'failed'][Math.floor(Math.random() * 2)] as
+        status: ['success', 'error'][Math.floor(Math.random() * 2)] as
           | 'success'
-          | 'failed',
+          | 'error',
       })
       setIsTrainingLoading(false)
     }, 6000)
   }
+
   const handleCancelTrainBtnClick = () => {
     setIsTraining(false)
     setIsTrainingLoading(false)
+  }
+
+  const handleNotifyMeBtnClick = () => {
+    // Sending mail to user google Email logic here...
+    setIsNotified(true)
   }
 
   return (
@@ -138,26 +169,22 @@ const IntentCatcherSidePanel = ({
       position={position}
       name='Intent Catcher'>
       <div className={s.intentCatcherSidePanel}>
-        <div
-          className={`${s.intentCatcherSidePanel__container} ${
-            !isTrainingLoading &&
-            isTraining &&
-            s.intentCatcherSidePanel__container_muted
-          }`}>
+        <div className={s.intentCatcherSidePanel__container}>
           <Button
             theme='secondary'
             long
             props={{
               onClick: handleAddIntentBtnClick,
-              disabled: isTraining,
+              disabled: isTraining || disabled,
             }}>
             <PlusIcon />
             Add Intent
           </Button>
           <IntentList>
-            {intentsMock.map(({ name, about, status }, i) => (
+            {intentsMock.map(({ id, name, about, status }, i) => (
               <IntentListItem
-                key={name + i}
+                key={id}
+                id={id}
                 name={name}
                 about={about}
                 status={status}
@@ -165,13 +192,47 @@ const IntentCatcherSidePanel = ({
               />
             ))}
           </IntentList>
-          {!isTraining && (
+          {!isTraining && !disabled && (
             <Button theme='primary' props={{ onClick: handleTrainBtnClick }}>
               Train
             </Button>
           )}
         </div>
 
+        {/* Training not available yet */}
+        {disabled && (
+          <SidePanelStatus
+            status='default'
+            title='Sorry, training is not yet available.'
+            desc='Stay tuned for the upcoming updates!'>
+            <Button
+              theme='secondary'
+              props={{ onClick: () => setIsOpen(false) }}>
+              Close
+            </Button>
+            <div data-tip data-for='notify-me'>
+              <Button
+                theme='primary'
+                props={{
+                  disabled: isNotified,
+                  onClick: handleNotifyMeBtnClick,
+                }}>
+                {isNotified ? '✓' : 'Notify me'}
+              </Button>
+            </div>
+            <ReactTooltip
+              place='bottom'
+              effect='solid'
+              className='tooltips'
+              arrowColor='#8d96b5'
+              delayShow={1000}
+              id='notify-me'>
+              via Email
+            </ReactTooltip>
+          </SidePanelStatus>
+        )}
+
+        {/* Training loading bottom info */}
         {isTrainingLoading && (
           <div className={s.training}>
             <span className={s.training__status}>Training</span>
@@ -192,35 +253,28 @@ const IntentCatcherSidePanel = ({
             </div>
           </div>
         )}
+
+        {/* Training status */}
         {!isTrainingLoading && isTraining && (
-          <div
-            className={`${s.trainingResponse} ${
-              s[`trainingResponse_${trainingResponse?.status}`]
-            }`}>
-            <div className={s.trainingResponse__status}>
-              Model training has{' '}
-              {trainingResponse?.status === 'success' ? 'succeeded' : 'failed'}.
-              <BaseLink to='#' theme='link'>
-                See details
-              </BaseLink>
-            </div>
-            <p className={s.trainingResponse__about}>
-              Do you want to replace the original Intent Catcher with this
-              model?
-            </p>
-            <div className={s.trainingResponse__btns}>
-              <Button
-                theme='secondary'
-                props={{ onClick: handleCancelTrainBtnClick }}>
-                Cancel
-              </Button>
-              <Button
-                theme='primary'
-                props={{ disabled: trainingResponse?.status === 'failed' }}>
-                Replace
-              </Button>
-            </div>
-          </div>
+          <SidePanelStatus
+            status={trainingResponse?.status ?? 'default'}
+            title={`Model training has ${
+              trainingResponse?.status === 'success' ? 'succeeded' : 'failed'
+            }.`}
+            desc='Do you want to replace the original Intent Catcher with this
+          model?'
+            detailsLink='#'>
+            <Button
+              theme='secondary'
+              props={{ onClick: handleCancelTrainBtnClick }}>
+              Cancel
+            </Button>
+            <Button
+              theme='primary'
+              props={{ disabled: trainingResponse?.status === 'error' }}>
+              Replace
+            </Button>
+          </SidePanelStatus>
         )}
 
         {/* Maybe need to organize here Edit/Add intent modals */}

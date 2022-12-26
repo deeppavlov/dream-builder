35095a9ac3413e6437d9499466e747935357d0d1
Timofey Syrov <warfacetopic@mail.ru>
2022-12-26 03:56:38 +0300

    added Bot, Skill, Event types

--- services/frontend/src/types/types.ts
+++ services/frontend/src/types/types.ts
@@ -10,3 +10,43 @@ export interface UserContext {
   login: (res: any) => void
   logout: () => void
 }
+
+export type CustomEventName = string
+
+export type CustomEventListener = (data: any) => void
+
+export interface BotInfoInterface {
+  name: string
+  author: string
+  desc: string
+  dateCreated: string
+  version: string
+  ram: string
+  gpu: string
+  space: string
+  annotators?: string[]
+  skills?: string[]
+}
+
+export type SkillType =
+  | 'fallbacks'
+  | 'retrieval'
+  | 'generative'
+  | 'q_a'
+  | 'script'
+  | 'script_with_nns'
+
+export interface SkillInfoInterface {
+  name: string
+  author: string
+  dateCreated: string
+  desc: string
+  version: string
+  ram: string
+  gpu: string
+  skillType: SkillType
+  space?: string
+  time?: string
+  botName?: string
+  executionTime?: string
+}

a3b5da2f5534d126f3ebd26fcbb684add0655c72
Timofey Syrov <warfacetopic@mail.ru>
2022-12-30 17:44:10 +0300

    added observer for ResourcesSidePanel

--- services/frontend/src/components/ResourcesSidePanel/ResourcesSidePanel.tsx
+++ services/frontend/src/components/ResourcesSidePanel/ResourcesSidePanel.tsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from 'react'
 import { SidePanelProps } from '../../ui/SidePanel/SidePanel'
 import Button from '../../ui/Button/Button'
 import BaseSidePanel from '../BaseSidePanel/BaseSidePanel'
 import { SmallTag } from '../SmallTag/SmallTag'
+import { subscribe, unsubscribe } from '../../utils/events'
+import { TotalResourcesInterface } from '../../types/types'
 import s from './ResourcesSidePanel.module.scss'
 
-const ResourcesSidePanel = ({
-  isOpen,
-  setIsOpen,
-  position,
-}: SidePanelProps) => {
+const ResourcesSidePanel = ({ position }: Partial<SidePanelProps>) => {
+  const [res, setRes] = useState<TotalResourcesInterface | null>(null)
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    subscribe('ResourcesSidePanel', handleEventUpdate)
+    return () => unsubscribe('ResourcesSidePanel', handleEventUpdate)
+  }, [])
+
+  const handleEventUpdate = (data: { detail: TotalResourcesInterface }) => {
+    setRes(data.detail)
+    setIsOpen(!isOpen)
+  }
+
   const handleCloseBtnClick = () => setIsOpen(false)
 
   return (
@@ -21,7 +33,7 @@ const ResourcesSidePanel = ({
         <div className={s.resourcesBlock}>
           <div className={s.resourcesBlock__header}>
             <span className={s.resourcesBlock__name}>Using proxy</span>
-            <SmallTag>30 containers</SmallTag>
+            <SmallTag>{res?.proxy.containers ?? 0} containers</SmallTag>
           </div>
           <p className={s.resourcesBlock__about}>
             Some information about proxy
@@ -29,22 +41,28 @@ const ResourcesSidePanel = ({
           <ul className={s.resourcesBlock__table}>
             <li>
               <span>RAM:</span>
-              <span className={s['resourcesBlock__table-count']}>0.0 GB</span>
+              <span className={s['resourcesBlock__table-count']}>
+                {res?.proxy.ram ?? '0.0 GB'}
+              </span>
             </li>
             <li>
               <span>GPU:</span>
-              <span className={s['resourcesBlock__table-count']}>0.0 GB</span>
+              <span className={s['resourcesBlock__table-count']}>
+                {res?.proxy.gpu ?? '0.0 GB'}
+              </span>
             </li>
             <li>
               <span>Disk space:</span>
-              <span className={s['resourcesBlock__table-count']}>0.0 GB</span>
+              <span className={s['resourcesBlock__table-count']}>
+                {res?.proxy.space ?? '0.0 GB'}
+              </span>
             </li>
           </ul>
         </div>
         <div className={s.resourcesBlock}>
           <div className={s.resourcesBlock__header}>
             <span className={s.resourcesBlock__name}>Custom</span>
-            <SmallTag>1 container</SmallTag>
+            <SmallTag>{res?.custom.containers ?? 0} container</SmallTag>
           </div>
           <p className={s.resourcesBlock__about}>
             You’ve changed Intent Catcher, this container will use memory:
@@ -52,15 +70,21 @@ const ResourcesSidePanel = ({
           <ul className={s.resourcesBlock__table}>
             <li>
               <span>RAM:</span>
-              <span className={s['resourcesBlock__table-count']}>5.0 GB</span>
+              <span className={s['resourcesBlock__table-count']}>
+                {res?.custom.ram ?? '0.0 GB'}
+              </span>
             </li>
             <li>
               <span>GPU:</span>
-              <span className={s['resourcesBlock__table-count']}>4.0 GB</span>
+              <span className={s['resourcesBlock__table-count']}>
+                {res?.custom.gpu ?? '0.0 GB'}
+              </span>
             </li>
             <li>
               <span>Disk space:</span>
-              <span className={s['resourcesBlock__table-count']}>7.0 GB</span>
+              <span className={s['resourcesBlock__table-count']}>
+                {res?.custom.space ?? '0.0 GB'}
+              </span>
             </li>
           </ul>
         </div>

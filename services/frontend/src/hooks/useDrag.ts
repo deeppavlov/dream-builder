b263fbe5bd1527a1339e2496b281c01d72a57bcd
Artem Klementev <o.klmntv@gmail.com>
2023-01-11 13:44:16 +0300

    fix drag

--- services/frontend/src/hooks/useDrag.ts
+++ services/frontend/src/hooks/useDrag.ts
@@ -1,33 +1,49 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 
-export const useDrag = ref => {
+export const useDrag = (ref: React.RefObject<HTMLElement>) => {
   useEffect(() => {
     if (!ref.current) return
     let isDown = false
+    let isScrolling = false
     let startX: number
     let scrollLeft: number
-
-    ref.current.addEventListener('mousedown', e => {
+    const handleClick = (e: MouseEvent) => {
       e.stopPropagation()
+      ref.current!.removeEventListener('click', handleClick)
+    }
+    const handleMouseDown = (e: MouseEvent) => {
       isDown = true
-      startX = e.pageX - ref.current.offsetLeft
-      scrollLeft = ref.current.scrollLeft
-    })
-    ref.current.addEventListener('mouseleave', () => {
+      startX = e.pageX - ref.current!.offsetLeft
+      scrollLeft = ref.current!.scrollLeft
+    }
+    const handleMouseLeave = () => (isDown = false)
+
+    const handleMouseUp = (e: MouseEvent) => {
       isDown = false
-    })
-    ref.current.addEventListener('mouseup', () => {
-      e.stopPropagation()
-      isDown = false
-    })
-    ref.current.addEventListener('mousemove', e => {
+      if (isScrolling) {
+        isScrolling = false
+        ref.current!.addEventListener('click', handleClick)
+      }
+    }
+    const handleMouseMove = (e: MouseEvent) => {
       if (!isDown) return
-      e.stopPropagation()
-      e.preventDefault()
-      const x = e.pageX - ref.current.offsetLeft
+      isScrolling = true
+      const x = e.pageX - ref.current!.offsetLeft
       const walk = x - startX
-      ref.current.scrollLeft = scrollLeft - walk
-    })
+      ref.current!.scrollLeft = scrollLeft - walk
+    }
+    ref.current.addEventListener('mousedown', handleMouseDown)
+    ref.current.addEventListener('mouseleave', handleMouseLeave)
+    ref.current.addEventListener('mouseup', handleMouseUp)
+    ref.current.addEventListener('mousemove', handleMouseMove)
+
+    return () => {
+      ref.current?.removeEventListener('click', handleClick)
+      ref.current?.removeEventListener('mousedown', handleMouseDown)
+      ref.current?.removeEventListener('mouseleave', handleMouseLeave)
+      ref.current?.removeEventListener('mouseup', handleMouseUp)
+      ref.current?.removeEventListener('mousemove', handleMouseMove)
+    }
   }, [ref])
   return ref
 }

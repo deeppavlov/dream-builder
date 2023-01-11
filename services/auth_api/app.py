43a122c166957522233f8024660cd82002496feb
mtalimanchuk <mtalimanchuk@gmail.com>
2023-01-11 14:12:05 +0400

    remove cors middleware, update api port

--- services/auth_api/app.py
+++ services/auth_api/app.py
@@ -1,16 +1,8 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 
 from services.auth_api.auth import router
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(router)

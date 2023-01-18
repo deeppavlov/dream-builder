62c404c0af7aedf9ed8c90bf50824be7653d73ac
Artem Klementev <o.klmntv@gmail.com>
2023-01-18 21:55:41 +0300

    fix service

--- services/frontend/src/services/getComponentsFromAssistantDists.ts
+++ services/frontend/src/services/getComponentsFromAssistantDists.ts
@@ -1,11 +1,10 @@
-import { getCookie } from './AuthProvider'
 import { api } from './axiosConfig'
 
 export async function getComponentsFromAssistantDists(distName: string) {
   try {
     const { data } = await api.get(`assistant_dists/${distName}/components/`, {
       headers: {
-        token: getCookie('jwt_token'),
+        token: localStorage.getItem('token'),
       },
     })
     return data

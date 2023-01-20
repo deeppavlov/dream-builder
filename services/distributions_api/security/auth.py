1126d65b0388d18b4328c643cf8a5701109af913
Azat Ilyazov <86655859+ilyazat@users.noreply.github.com>
2023-01-20 13:23:43 +0300

    add detalization of auth error (#31)

--- services/distributions_api/security/auth.py
+++ services/distributions_api/security/auth.py
@@ -4,10 +4,11 @@ from fastapi import Header, HTTPException
 from services.distributions_api.config import settings
 
 
-async def verify_token(jwt_data: str = Header()):
-    header = {"jwt-data": jwt_data}
+async def verify_token(token: str = Header()):
+    header = {"token": token}
 
     async with aiohttp.ClientSession(headers=header) as session:
         async with session.get(f"{settings.url.auth_api}/auth/token") as response:
             if response.status != 200:
-                raise HTTPException(status_code=400, detail="bad token")
+                json_data = await response.json()
+                raise HTTPException(status_code=400, detail=json_data["detail"])

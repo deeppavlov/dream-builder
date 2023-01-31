ec48b2ffec5539f97a7630cff12580ef5b9043b6
Timofey Syrov <warfacetopic@mail.ru>
2023-01-31 12:55:46 +0300

    fixed requests error display

--- services/frontend/src/services/axiosConfig.ts
+++ services/frontend/src/services/axiosConfig.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { trigger } from '../utils/events'
 import { deleteLocalStorageUser } from './AuthProvider'
 
@@ -6,10 +6,13 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 })
 
-const handleBadTokenResponse = () => {
+const handleErrorResponse = (error: AxiosError) => {
   localStorage.removeItem('token')
   deleteLocalStorageUser()
-  trigger('ErrorMessageModal', 'bad token')
+  trigger(
+    'ErrorMessageModal',
+    `${JSON.stringify(error?.response?.data, undefined, 2)}`
+  )
 }
 
 // Add a request interceptor
@@ -32,15 +35,17 @@ api.interceptors.response.use(
 
     return response
   },
-  function (error) {
+  function (error: AxiosError) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    if (
-      error.response.status === 400 &&
-      error.response.data?.detail === 'bad token'
-    ) {
-      handleBadTokenResponse()
+    const tokenIsLost =
+      error.config?.headers?.token === null ||
+      error.config?.headers?.token === undefined
+
+    if (error?.response?.status === 400 || tokenIsLost) {
+      handleErrorResponse(error)
     }
+
     return Promise.reject(error)
   }
 )

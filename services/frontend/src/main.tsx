d78cf5e7e9a0f64c3928b0860718f8c314313e8b
Artem Klementev <o.klmntv@gmail.com>
2023-02-21 11:41:32 +0300

    fix msw mount

--- services/frontend/src/main.tsx
+++ services/frontend/src/main.tsx
@@ -1,28 +1,25 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthProvider'
-import { App } from './App'
 import { PreviewProvider } from './context/PreviewProvider'
+import { App } from './App'
 
-function prepare() {
+async function prepare() {
   if (process.env.NODE_ENV === 'development') {
-    import('./mocks/browser').then(({ worker }) => {
+    return import('./mocks/browser').then(({ worker }) => {
       worker.start()
     })
   }
-  return Promise.resolve()
 }
 
 prepare().then(() => {
-  setTimeout(() => {
-    ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-      <BrowserRouter>
-        <AuthProvider>
-          <PreviewProvider>
-            <App />
-          </PreviewProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    )
-  }, 100)
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <BrowserRouter>
+      <AuthProvider>
+        <PreviewProvider>
+          <App />
+        </PreviewProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  )
 })

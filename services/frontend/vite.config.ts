4d453278bed1923c4dbe78ac3a7c231fc64018ae
Artem Klementev <o.klmntv@gmail.com>
2023-03-02 11:55:23 +0300

    new config for vite

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'node:url'
 import svgr from 'vite-plugin-svgr'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), chunkSplitPlugin()],
   css: {
     preprocessorOptions: {
       scss: { additionalData: `@import "./src/styles/index";` },

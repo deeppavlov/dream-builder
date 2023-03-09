02b4e587adb350c5b4c175190bd1efbfb2d852a6
Artem Klementev <o.klmntv@gmail.com>
2023-03-09 10:47:40 +0300

    fix requests (add error throw & fix type of api)

--- services/frontend/src/services/publishUsersAssistantDist.ts
+++ services/frontend/src/services/publishUsersAssistantDist.ts
@@ -1,12 +1,14 @@
 import axios from 'axios'
 import { privateApi } from './axiosConfig'
-//replace axios.post with privateApi.post to interract with real endpoint
 
 export async function publishAssistantDist(dist_name: string) {
   try {
-    const { data } = await axios.post(`/assistant_dists/${dist_name}/publish`)
+    const { data } = await privateApi.post(
+      `/assistant_dists/${dist_name}/publish`
+    )
     return data
   } catch (e) {
     console.log(e)
+    throw e
   }
 }

3cc2606975a95d4b27aecae0c636d1064e64a5d9
Artem Klementev <o.klmntv@gmail.com>
2023-03-09 10:59:15 +0300

    refactoring toggle button

--- services/frontend/src/ui/ToggleButton/ToggleButton.tsx
+++ services/frontend/src/ui/ToggleButton/ToggleButton.tsx
@@ -3,19 +3,19 @@ import classNames from 'classnames/bind'
 import s from './ToggleButton.module.scss'
 
 interface ToggleProps {
-  sliderHandler: () => void
+  handleToggle: () => void
   disabled?: boolean
 }
 
-export const ToggleButton: FC<ToggleProps> = ({ sliderHandler, disabled }) => {
+export const ToggleButton: FC<ToggleProps> = ({ handleToggle, disabled }) => {
   const clickHandler = (e: React.MouseEvent) => {
     e.stopPropagation()
-    sliderHandler()
+    handleToggle()
   }
   const cx = classNames.bind(s)
   return (
     <label className={s.switch} onClick={clickHandler}>
-      <input onClick={sliderHandler} disabled={disabled} type='checkbox' />
+      <input onClick={handleToggle} disabled={disabled} type='checkbox' />
       <span className={cx('slider', disabled && 'disabled')} />
     </label>
   )

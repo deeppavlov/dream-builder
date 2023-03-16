ccafc5120ed887546567bea27426d1aa64e9401a
Artem Klementev <o.klmntv@gmail.com>
2023-03-16 15:21:37 +0300

    add triggers for skillListModal

--- services/frontend/src/ui/AddButton/AddButton.tsx
+++ services/frontend/src/ui/AddButton/AddButton.tsx
@@ -1,17 +1,35 @@
+import classNames from 'classnames/bind'
 import { FC } from 'react'
 import Add from '../../assets/icons/+.svg'
+import { useAuth } from '../../context/AuthProvider'
+import { usePreview } from '../../context/PreviewProvider'
 import { trigger } from '../../utils/events'
 import s from './AddButton.module.scss'
+import { mockSkills } from '../../mocks/database/mockSkills'
 
 interface Props {
   text?: string
-  listView?: boolean
   disabled?: boolean
+  forTable?: boolean
+  forGrid?: boolean
+  forSkills?: boolean
+  fromScratch?: boolean
 }
 
-export const AddButton: FC<Props> = ({ text, listView, disabled }) => {
+export const AddButton: FC<Props> = ({
+  text,
+  disabled,
+  forGrid,
+  forTable,
+  forSkills,
+  fromScratch,
+}) => {
+  const cx = classNames.bind(s)
+  const auth = useAuth()
+  const { isPreview } = usePreview()
+
   const handleClick = () => {
-    if (disabled) {
+    if (!auth?.user) {
       trigger('SignInModal', {})
       return
     }
@@ -20,15 +38,23 @@ export const AddButton: FC<Props> = ({ text, listView, disabled }) => {
       trigger('AssistantModal', { action: 'create' })
     }
 
-    addBot()
+    if (forSkills && !isPreview) trigger('SkillsListModal', { mockSkills })
+    fromScratch && alert('this for create skills from scratch')
+    if (!forSkills && !fromScratch) addBot()
   }
 
-  return !listView ? (
-    <button onClick={handleClick} className={s.forCard}>
+  return !forTable ? (
+    <button
+      onClick={handleClick}
+      className={cx(
+        'forCard',
+        forGrid && 'forGrid'
+        // disabled && 'disabled'
+      )}>
       <img src={Add} />
     </button>
   ) : (
-    <tr className={s.tr}>
+    <tr className={cx('tr', disabled && 'disabled')}>
       <td colSpan={5} className={s.td}>
         <button className={s.forTable} onClick={handleClick}>
           <img src={Add} />

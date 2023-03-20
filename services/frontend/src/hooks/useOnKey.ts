3eb7a38a2d9bc8d7e5a02d2c9094c432caf2c35d
Artem Klementev <o.klmntv@gmail.com>
2023-03-20 11:54:41 +0300

    new hook

--- services/frontend/src/hooks/useOnKey.ts
+++ services/frontend/src/hooks/useOnKey.ts
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 export const useOnKey = (func: any, key: string) => {
   const keyDownHandler = event => {
     event.key === key && func()
+    event.stopPropagation()
   }
 
   useEffect(() => {

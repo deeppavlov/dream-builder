a0f0f805d0e5949ced477d9dd8ffe33b194b178b
Artem Klementev <o.klmntv@gmail.com>
2023-03-29 15:13:02 +0300

    fix publish endpoint

--- services/frontend/src/services/publishUsersAssistantDist.ts
+++ services/frontend/src/services/publishUsersAssistantDist.ts
@@ -1,10 +1,15 @@
-import axios from 'axios'
+import { BotVisabilityType } from '../types/types'
 import { privateApi } from './axiosConfig'
 
-export async function publishAssistantDist(dist_name: string) {
+export async function publishAssistantDist(
+  dist_name: string,
+  hide: boolean,
+  visability: BotVisabilityType
+) {
   try {
     const { data } = await privateApi.post(
-      `/assistant_dists/${dist_name}/publish`
+      `/assistant_dists/${dist_name}/publish/`,
+      { hide, visability }
     )
     return data
   } catch (e) {

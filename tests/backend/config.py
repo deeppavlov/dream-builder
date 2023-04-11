c81ecaef8cba62af4bc9fbddad6eceb3082e2f52
NikitaMartynenko <niyoko4@mail.ru>
2023-04-11 12:46:54 +0300

    added utility functions

--- tests/backend/config.py
+++ tests/backend/config.py
@@ -1,16 +1,26 @@
 from pydantic import BaseModel, BaseSettings
+from pathlib import Path
 from services.auth_api.models import UserCreate, UserValidScheme
 from database import models
 from sqlalchemy.orm import Session
+from sqlalchemy.dialects.postgresql import insert
 
 
-class UrlSettings(BaseModel):
+class LocalUrlSettings(BaseModel):
     frontend: str
     auth_api: str
     distributions_api: str
 
 
-class TestUrlSettings(BaseModel):
+class LocalDatabaseSettings(BaseModel):
+    user: str
+    password: str
+    host: str
+    port: int
+    name: str
+
+
+class UrlSettings(BaseModel):
     frontend: str
     auth_api: str
     distributions_api: str
@@ -24,60 +34,95 @@ class DatabaseSettings(BaseModel):
     name: str
 
 
-class TestDatabaseSettings(BaseModel):
-    user: str
-    password: str
-    host: str
-    port: int
-    name: str
-
-
 class AuthSettings(BaseModel):
     google_client_id: str
     test_token: str
+    sub: str
+    chat_url: str
 
 
 class Settings(BaseSettings):
     url: UrlSettings
-    testurl: TestUrlSettings
+    localurl: LocalUrlSettings
     db: DatabaseSettings
-    testdb: TestDatabaseSettings
+    localdb: LocalDatabaseSettings
     auth: AuthSettings
 
     class Config:
-        env_file = ".env"
+        env_file = (Path(__file__).parents[2] / ".env").absolute()
         env_file_encoding = "utf-8"
         env_nested_delimiter = "__"
 
 
 settings = Settings()
+auth_token = settings.auth.test_token
+# remove 'local' if we run tests not locally
+settings_url = settings.localurl
+settings_db = settings.localdb
+
+# config for test_auth
+
+
+auth_endpoint = f"{settings_url.auth_api}/"
+auth_refresh_token = "1refresh_token"
+
 
 # config for test_db
 
+
 def clean_testdb(db: Session):
+    db.query(models.DialogSession).delete(synchronize_session="fetch")
+    db.query(models.PublishRequest).delete(synchronize_session="fetch")
+    db.query(models.UserApiToken).delete(synchronize_session="fetch")
     db.query(models.Deployment).delete(synchronize_session="fetch")
     db.query(models.VirtualAssistant).delete(synchronize_session="fetch")
     db.query(models.UserValid).delete(synchronize_session="fetch")
     db.query(models.GoogleUser).delete(synchronize_session="fetch")
+    db.query(models.LmService).delete(synchronize_session="fetch")
+    db.query(models.ApiToken).delete(synchronize_session="fetch")
+    db.query(models.Role).delete(synchronize_session="fetch")
     db.commit()
 
 
-def clean_testdata(db: Session, email):
+def clean_testdata_wo_user(db: Session, email):
     google_user_id = db.query(models.GoogleUser.id).filter(models.GoogleUser.email == email).first()[0]
-    va_id = db.query(models.VirtualAssistant.id).filter(models.VirtualAssistant.author_id == google_user_id).first()[0]
-    db.query(models.Deployment).filter(models.Deployment.virtual_assistant_id == va_id).delete(synchronize_session="fetch")
-    db.query(models.VirtualAssistant).filter(models.VirtualAssistant.author_id == google_user_id).delete(synchronize_session="fetch")
+    va_id_list = db.query(models.VirtualAssistant.id).filter(models.VirtualAssistant.author_id == google_user_id).all()
+    for va_id in va_id_list:
+        db.query(models.DialogSession).filter(models.DialogSession.user_id == 1).delete(synchronize_session="fetch")
+        db.query(models.Deployment).filter(models.Deployment.virtual_assistant_id == va_id[0]).delete(
+            synchronize_session="fetch"
+        )
+    db.query(models.PublishRequest).filter(models.PublishRequest.user_id == google_user_id).delete(
+        synchronize_session="fetch"
+    )
+    db.query(models.UserApiToken).filter(models.UserApiToken.user_id == google_user_id).delete(
+        synchronize_session="fetch"
+    )
+    db.query(models.DialogSession).filter(models.DialogSession.user_id == google_user_id).delete(
+        synchronize_session="fetch"
+    )
+    db.query(models.VirtualAssistant).filter(models.VirtualAssistant.author_id == google_user_id).delete(
+        synchronize_session="fetch"
+    )
+    db.commit()
+    db.close()
+
+
+def clean_all_testdata(db: Session, email):
+    clean_testdata_wo_user(db, email)
+    google_user_id = db.query(models.GoogleUser.id).filter(models.GoogleUser.email == email).first()[0]
     db.query(models.UserValid).filter(models.UserValid.user_id == google_user_id).delete(synchronize_session="fetch")
     db.query(models.GoogleUser).filter(models.GoogleUser.email == email).delete(synchronize_session="fetch")
     db.commit()
+    db.close()
 
 
 db_config = (
-    settings.testdb.user,
-    settings.testdb.password,
-    settings.testdb.host,
-    settings.testdb.port,
-    settings.testdb.name,
+    settings_db.user,
+    settings_db.password,
+    settings_db.host,
+    settings_db.port,
+    settings_db.name,
 )
 
 exist_email = "existemail@gmail.com"
@@ -99,7 +144,6 @@ json_google_user = f"""{{
                          }}"""
 google_user = UserCreate.parse_raw(json_google_user)
 
-
 json_uservalid = f"""{{
                      "refresh_token" : "{refresh_token}", 
                      "is_valid" : "true", 
@@ -114,66 +158,26 @@ json_uservalid_updated = f"""{{
                                }}"""
 uservalid_user_updated = UserValidScheme.parse_raw(json_uservalid_updated)
 
-json_user_api_token = f"""{{ 
-                         "id" : "{exist_email}", 
-                         "user_id" : "{exist_sub}",
-                         "api_token_id" : "api_token_id", 
-                         "token_value" : "token_value",
-                         }}"""
+# json_user_api_token = f"""{{
+#                         "id" : "{exist_email}",
+#                         "user_id" : "{exist_sub}",
+#                         "api_token_id" : "api_token_id",
+#                         "token_value" : "token_value",
+#                         }}"""
 
 va_data = {
-           "cloned_from_id": None,
-           "author_id": "1",
-           "source": "source",
-           "name": "Test",
-           "display_name": "Test_Assistant",
-           "description": "description"
-            }
-
-
-# config for test_auth
-
-
-header_token = {
-    "token": settings.auth.test_token,
-    "accept": "application/json"
+    "cloned_from_id": None,
+    "author_id": 1,
+    "source": "source",
+    "name": "TestVA",
+    "display_name": "Test_Assistant",
+    "description": "description",
 }
-header_logout = headers = {
-    "accept": "*/*",
-    "refresh-token": f"{refresh_token}",
-}
-header_exchange_authcode = {
-    'accept': 'application/json',
-    'content-type': 'application/x-www-form-urlencoded',
-}
-params_exchange_authcode = {
-    'auth_code': f'{settings.auth.test_token}',
-}
-params_exchange_bad_authcode = {
-    'auth_code': f'{settings.auth.test_token}',
-}
-header_update_token = {
-    'accept': 'application/json',
-    'content-type': 'application/x-www-form-urlencoded',
-}
-
-params_update_token = {
-    'refresh_token': f"{refresh_token}",
-}
-
-
-
-token_endpoint = f"{settings.testurl.auth_api}/token"
-logout_endpoint = f"{settings.testurl.auth_api}/logout"
-exchange_authcode_endpoint = f"{settings.testurl.auth_api}/exchange_authcode"
-update_token_endpoint = f"{settings.testurl.auth_api}/update_token"
-
 
 # config for test_distribution
-base_url_distributions_api = settings.testurl.distributions_api
 
-auth_token = settings.auth.test_token
 
+base_url_distributions_api = settings_url.distributions_api
 
 assistant_dists_endpoint = f"{base_url_distributions_api}/assistant_dists/"
 users_endpoint = f"{base_url_distributions_api}/users/"
@@ -182,3 +186,147 @@ dialog_sessions_endpoint = f"{base_url_distributions_api}/dialog_sessions/"
 deployments_endpoint = f"{base_url_distributions_api}/deployments/"
 
 
+def create_mocks_public_dist(db: Session):
+    db.scalar(
+        insert(models.Role)
+            .values(
+            id=1,
+            name="user",
+            can_confirm_publish=True,
+            can_set_roles=True,
+        )
+            .returning(models.Role)
+    )
+
+    db.scalar(
+        insert(models.LmService)
+            .values(
+            id=1,
+            name="GPT-J 6B",
+            display_name="GPT-J 6B",
+            description="description",
+            project_url="project_url",
+        )
+            .returning(models.LmService)
+    )
+
+    db.scalar(
+        insert(models.LmService)
+            .values(
+            id=2,
+            name="BLOOMZ 7B",
+            display_name="BLOOMZ 7B",
+            description="description",
+            project_url="project_url",
+        )
+            .returning(models.LmService)
+    )
+
+    db.scalar(
+        insert(models.LmService)
+            .values(
+            id=3,
+            name="GPT-3.5",
+            display_name="GPT-3.5",
+            description="description",
+            project_url="project_url",
+        )
+            .returning(models.LmService)
+    )
+
+    db.scalar(
+        insert(models.LmService)
+            .values(
+            id=4,
+            name="ChatGPT",
+            display_name="ChatGPT",
+            description="description",
+            project_url="project_url",
+        )
+            .returning(models.LmService)
+    )
+
+    db.scalar(
+        insert(models.GoogleUser)
+            .values(
+            id=1,
+            email="publisher@testmail.com",
+            sub=settings.auth.sub,
+            picture="picture",
+            fullname="publisher",
+            given_name="publisher",
+            family_name="publisher",
+            role_id=1,
+        )
+            .returning(models.GoogleUser)
+    )
+
+    db.scalar(
+        insert(models.VirtualAssistant)
+            .values(
+            id=1,
+            cloned_from_id=None,
+            author_id=1,
+            source="source",
+            name="fairytale_assistant",
+            display_name="fairytale_assistant",
+            description="description",
+            date_created="2023-04-10 13:28:46.543905",
+        )
+            .returning(models.VirtualAssistant)
+    )
+
+    db.scalar(
+        insert(models.ApiToken)
+            .values(
+            id=1,
+            name="xGPT",
+            description="description",
+            base_url="base_url",
+        )
+            .returning(models.ApiToken)
+    )
+
+    db.scalar(
+        insert(models.Deployment)
+            .values(
+            id=1,
+            virtual_assistant_id=1,
+            chat_url=settings.auth.chat_url,
+            prompt="You tell fairy tales",
+            lm_service_id=3,
+        )
+            .returning(models.Deployment)
+    )
+
+    db.scalar(
+        insert(models.PublishRequest)
+            .values(
+            id=1,
+            slug="fairytale_assistant",
+            date_created="2023-04-10 13:28:46.543905",
+            virtual_assistant_id=1,
+            user_id=1,
+            is_confirmed=True,
+            confirmed_by_user_id=1,
+        )
+            .returning(models.PublishRequest)
+    )
+
+    db.commit()
+
+
+def create_counter(title: str):
+    i = 0
+
+    def func():
+        nonlocal i
+        i += 1
+        return f'{title}: {i}'
+
+    return func
+
+
+counter_auth = create_counter('TestAuth')
+counter_distributions = create_counter('TestDistributions')
+counter_db = create_counter('TestDB')

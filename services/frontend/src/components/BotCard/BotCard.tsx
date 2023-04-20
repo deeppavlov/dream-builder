c4da5ca4d5cbebabd95d5b1f190d2084647b0768
Artem Klementev <o.klmntv@gmail.com>
2023-04-20 21:54:06 +0300

    delete author

--- services/frontend/src/components/BotCard/BotCard.tsx
+++ services/frontend/src/components/BotCard/BotCard.tsx
@@ -1,27 +1,24 @@
-import { useId, useRef } from 'react'
+import { ReactComponent as CalendarIcon } from '@assets/icons/calendar.svg'
+import { ReactComponent as PreviewIcon } from '@assets/icons/eye.svg'
 import classNames from 'classnames/bind'
+import { useId, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { trigger } from '../../utils/events'
+import { useDisplay } from '../../context/DisplayContext'
 import {
   BotAvailabilityType,
   BotCardSize,
   BotInfoInterface,
 } from '../../types/types'
-import { ReactComponent as CalendarIcon } from '@assets/icons/calendar.svg'
-import { ReactComponent as PreviewIcon } from '@assets/icons/eye.svg'
 import Button from '../../ui/Button/Button'
-import DeepPavlovLogo from '@assets/icons/deeppavlov_logo_round.svg'
-import Woman from '../../assets/icons/woman.png'
 import { Kebab } from '../../ui/Kebab/Kebab'
-import { TRIGGER_RIGHT_SP_EVENT } from '../BaseSidePanel/BaseSidePanel'
-import BotInfoSidePanel from '../BotInfoSidePanel/BotInfoSidePanel'
-import BotCardToolTip from '../BotCardToolTip/BotCardToolTip'
-import BaseToolTip from '../BaseToolTip/BaseToolTip'
-import { useDisplay } from '../../context/DisplayContext'
 import { consts } from '../../utils/consts'
 import { dateToUTC } from '../../utils/dateToUTC'
+import { trigger } from '../../utils/events'
+import { TRIGGER_RIGHT_SP_EVENT } from '../BaseSidePanel/BaseSidePanel'
+import BaseToolTip from '../BaseToolTip/BaseToolTip'
+import BotCardToolTip from '../BotCardToolTip/BotCardToolTip'
+import BotInfoSidePanel from '../BotInfoSidePanel/BotInfoSidePanel'
 import s from './BotCard.module.scss'
-
 
 interface BotCardProps {
   type: BotAvailabilityType
@@ -90,11 +87,12 @@ export const BotCard = ({ type, bot, size, disabled }: BotCardProps) => {
       className={cx('botCard', `${type}`, size)}
       onClick={handleBotCardClick}
       ref={botCardRef}
-      data-active={bot.name === activeAssistantId}>
+      data-active={bot.name === activeAssistantId}
+    >
       <div className={s.header}>{bot?.display_name}</div>
       <div className={s.body}>
         <div className={s.block}>
-          {type === 'public' && (
+          {/* {type === 'public' && (
             <div className={s.author}>
               <img referrerPolicy='no-referrer' src={Woman} />
               <span>
@@ -103,7 +101,7 @@ export const BotCard = ({ type, bot, size, disabled }: BotCardProps) => {
                   : bot?.author?.fullname!}
               </span>
             </div>
-          )}
+          )} */}
           <div className={s.desc} data-tooltip-id={'botCardDesc' + bot?.name}>
             {bot?.description}
             <BaseToolTip
@@ -127,12 +125,14 @@ export const BotCard = ({ type, bot, size, disabled }: BotCardProps) => {
               <div
                 data-tip
                 data-tooltip-id={'botClone' + bot?.name}
-                className={s.container}>
+                className={s.container}
+              >
                 <Button
                   theme='primary'
                   small
                   long
-                  props={{ onClick: handleCloneClick }}>
+                  props={{ onClick: handleCloneClick }}
+                >
                   Use
                 </Button>
               </div>
@@ -140,7 +140,8 @@ export const BotCard = ({ type, bot, size, disabled }: BotCardProps) => {
                 theme='secondary'
                 small
                 withIcon
-                props={{ onClick: handlePreviewClick }}>
+                props={{ onClick: handlePreviewClick }}
+              >
                 <PreviewIcon />
               </Button>
             </>
@@ -150,7 +151,8 @@ export const BotCard = ({ type, bot, size, disabled }: BotCardProps) => {
                 theme='primary'
                 small
                 long
-                props={{ onClick: handlEditClick }}>
+                props={{ onClick: handlEditClick }}
+              >
                 Edit
               </Button>
 

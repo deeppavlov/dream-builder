b3f7dfead0862e01b261d1ced6aaf264dec1b417
Timofei Syromiatnikov <warfacetopic@mail.ru>
2023-05-01 12:23:45 +0300

    fixed getting lm_services

--- services/frontend/src/services/getAllLMservices.ts
+++ services/frontend/src/services/getAllLMservices.ts
@@ -2,7 +2,7 @@ import { api } from './axiosConfig'
 
 export async function getAllLMservices() {
   try {
-    const { data } = await api.get(`deployments/lm_services`)
+    const { data } = await api.get(`lm_services`)
     return data
   } catch (e) {
     throw e

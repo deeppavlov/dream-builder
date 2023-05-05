d318164e1d7b0452466219e9cec8ff86618fb218
mtalimanchuk <mtalimanchuk@gmail.com>
2023-05-05 21:14:35 +0400

    fix invisible dists

--- services/distributions_api/const.py
+++ services/distributions_api/const.py
@@ -3,6 +3,8 @@ TEMPLATE_DIST_PROMPT_BASED = "dream_persona_prompted"
 INVISIBLE_VIRTUAL_ASSISTANT_NAMES = [
     "universal_prompted_assistant",
     "deepy_assistant",
+    "deeppavlov_assistant",
     "dream_persona_prompted",
     "dream_persona_openai_prompted",
+    "dream_persona_rullama_prompted",
 ]

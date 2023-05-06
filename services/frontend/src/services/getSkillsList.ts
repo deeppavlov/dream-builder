94fbc08a6c9f67b85512d1c28a392445cc2f7e22
Artem Klementev <o.klmntv@gmail.com>
2023-05-06 11:46:08 +0300

    update  services

--- services/frontend/src/services/getSkillsList.ts
+++ services/frontend/src/services/getSkillsList.ts
@@ -1,6 +1,6 @@
 import { api } from './axiosConfig'
 
 export async function getSkillList() {
-  const { data } = await api.get('skills/')
+  const { data } = await api.get('skills')
   return data
 }

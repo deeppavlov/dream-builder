58cd93aee91af6ee07f5e2ca913503c28dde4a79
Artem Klementev <o.klmntv@gmail.com>
2023-05-26 11:08:11 +0300

    fix switch

--- services/frontend/src/components/SwitchLanguageButton/SwitchLanguageButton.tsx
+++ services/frontend/src/components/SwitchLanguageButton/SwitchLanguageButton.tsx
@@ -7,10 +7,12 @@ import s from './SwitchLanguageButton.module.scss'
 
 export const SwitchLanguageButton = () => {
   const cx = classNames.bind(s)
+  const { i18n } = useTranslation()
 
   const activeLocale: ELOCALES_KEY = store(I18N_STORE_KEY) || ELOCALES_KEY.en
 
-  const { i18n } = useTranslation()
+  const enActive = activeLocale === ELOCALES_KEY.en
+  const ruActive = activeLocale === ELOCALES_KEY.ru
 
   const enHandler = () => i18n.changeLanguage(ELOCALES_KEY.en)
   const ruHandler = () => i18n.changeLanguage(ELOCALES_KEY.ru)
@@ -19,21 +21,13 @@ export const SwitchLanguageButton = () => {
     <div className={s.container}>
       <button
         onClick={enHandler}
-        className={cx(
-          'switch',
-          'left',
-          activeLocale === ELOCALES_KEY.en && 'active'
-        )}
+        className={cx('switch', 'left', { active: enActive })}
       >
         {ELOCALES_TITLE.EN}
       </button>
       <button
         onClick={ruHandler}
-        className={cx(
-          'switch',
-          'right',
-          activeLocale === ELOCALES_KEY.ru && 'active'
-        )}
+        className={cx('switch', 'right', { active: ruActive })}
       >
         {ELOCALES_TITLE.RU}
       </button>

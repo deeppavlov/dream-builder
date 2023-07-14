a055586f4c30d75aa7ac860950879c40f7b4d01d
mtalimanchuk <mtalimanchuk@gmail.com>
2023-07-14 18:55:45 +0400

    add prompt blocks

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -3,6 +3,8 @@ from database.models.component.model import Component
 from database.models.deployment.model import Deployment
 from database.models.dialog_session.model import DialogSession
 from database.models.lm_service.model import LmService
+from database.models.lm_service_prompt_block.model import LmServicePromptBlock
+from database.models.prompt_block.model import PromptBlock
 from database.models.publish_request.model import PublishRequest
 from database.models.role.model import Role
 from database.models.service.model import Service
@@ -10,4 +12,3 @@ from database.models.user.model import GoogleUser
 from database.models.uservalid.model import UserValid
 from database.models.virtual_assistant.model import VirtualAssistant
 from database.models.virtual_assistant_component.model import VirtualAssistantComponent
-

c4a77fca17b1735c222e34fa4c8ca1978b7437de
Artem M <artemmarin91@mail.ru>
2023-07-17 02:49:51 +0500

    updated node.js script

--- services/frontend/src/assets/scripts/node.js
+++ services/frontend/src/assets/scripts/node.js
@@ -1,27 +1,24 @@
-const fetch = require('node-fetch')
-
 // Endpoint 1: Create dialog session
-const createDialogSession = async () => {
+const createDialogSession = async (virtualAssistantName) => {
   const url = 'https://hostname/api/dialog_sessions'
   const payload = {
-    virtual_assistant_name: 'assistantName',
+    virtual_assistant_name: virtualAssistantName,
   }
 
   const response = await fetch(url, {
     method: 'POST',
     body: JSON.stringify(payload),
+    headers: { 'Content-Type': 'application/json' },
   })
 
   const data = await response.json()
-  const dialogSessionId = data.dialog_session_id
+  const dialogSessionId = data.id
+  console.log('Dialog session ID:', dialogSessionId)
+  return dialogSessionId
 }
 
 // Endpoint 2: Send a chat message
-const sendChatMessage = async (
-  dialogSessionId,
-  yourMessage,
-  openaiApiKey
-) => {
+const sendChatMessage = async (dialogSessionId, yourMessage, openaiApiKey) => {
   const url = `https://hostname/api/dialog_sessions/${dialogSessionId}/chat`
   const payload = {
     text: yourMessage,
@@ -31,38 +28,39 @@ const sendChatMessage = async (
   const response = await fetch(url, {
     method: 'POST',
     body: JSON.stringify(payload),
+    headers: { 'Content-Type': 'application/json' },
   })
 
   const data = await response.json()
+  const assistantMessage = data.text
+  console.log('Assistant message:', assistantMessage)
   // Handle the response data as needed
 }
 
 // Endpoint 3: Get dialog session history
-const getDialogSessionHistory = async dialogSessionId => {
+const getDialogSessionHistory = async (dialogSessionId) => {
   const url = `https://hostname/api/dialog_sessions/${dialogSessionId}/history`
-  const response = await fetch(url)
+  const response = await fetch(url, {
+    headers: { 'Content-Type': 'application/json' },
+  })
   const data = await response.json()
+  return data
   // Handle the response data as needed
 }
 
 // Usage example
-const dialogSessionId = 42 // Replace with the actual dialog session ID
+const virtualAssistantName = 'assistantName' // assistant must be builded
+const yourMessage = 'Hello World!'
+const openaiApiKey = 'sk-(the rest of your OpenAI key)'
+try {
+  const dialogSessionId = await createDialogSession(virtualAssistantName)
+  // or replace with the actual dialog session ID
 
-createDialogSession()
-  .then(() => {
-  const yourMessage = 'Hello World!'
+  await sendChatMessage(dialogSessionId, yourMessage, openaiApiKey)
 
-    const openaiApiKey = 'sk-(the rest of your OpenAI key)'
-
-    return sendChatMessage(
-      dialogSessionId,
-      yourMessage,
-      openaiApiKey
-    )
-  })
-  .then(() => {
-    return getDialogSessionHistory(dialogSessionId)
-  })
-  .catch(error => {
-    console.error('Error:', error)
-  })
+  const history = await getDialogSessionHistory(dialogSessionId)
+  console.log(history)
+  // Handle the session history data as needed
+} catch (error) {
+  console.log('Error: ', error)
+}

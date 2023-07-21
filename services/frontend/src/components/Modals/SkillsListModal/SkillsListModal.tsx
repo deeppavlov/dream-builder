aa47756a63f6c286a49afa7adc4d3b38f385de63
Artem Marin <artemmarin91@mail.ru>
2023-07-21 19:06:26 +0500

    fix animation (#96)

--- services/frontend/src/components/Modals/SkillsListModal/SkillsListModal.tsx
+++ services/frontend/src/components/Modals/SkillsListModal/SkillsListModal.tsx
@@ -96,11 +96,8 @@ export const SkillsListModal = () => {
       isOpen={isOpen}
       setIsOpen={setIsOpen}
       handleClose={handleClose}
-      modalClassName={s.modal}
-      backdropClassName={cx(
-        'backdrop',
-        rightSidepanelIsActive && 'withRightSidePanel'
-      )}
+      modalClassName={cx('modal', rightSidepanelIsActive && 'withRightSidePanel')}
+      backdropClassName={s.backdrop}
     >
       <div className={s.container}>
         <div className={s.header}>{t('modals.choose_skill.header')}</div>

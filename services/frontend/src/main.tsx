032473012ae3cbb7c7507f33d188fb14912ffa1c
Artem M <artemmarin91@mail.ru>
2023-07-26 12:50:13 +0500

    added react-ga4 lib

--- services/frontend/src/main.tsx
+++ services/frontend/src/main.tsx
@@ -2,12 +2,14 @@ import { App } from 'App'
 import { AuthProvider, PreviewProvider, UIOptionsProvider } from 'context'
 import { prepare } from 'mocks/prepare'
 import ReactDOM from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { RouterProvider } from 'react-router-dom'
 import { router } from 'router/Router'
 import './i18n'
 
 const queryClient = new QueryClient()
+ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS)
 
 prepare().then(() => {
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(

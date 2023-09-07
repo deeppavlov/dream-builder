9bd2253bea4cf67247ad8ece368daa9c01475e98
Artem Marin <artelis57@gmail.com>
2023-09-07 14:39:13 +0500

    fix github client id (#137)

--- services/frontend/src/api/user/getGithubOAuthURL.ts
+++ services/frontend/src/api/user/getGithubOAuthURL.ts
@@ -2,7 +2,9 @@ export const getGitHubOAuthURL = () => {
   const rootUrl = 'https://github.com/login/oauth/authorize'
 
   const options = {
-    client_id: import.meta.env.VITE_GITHUB_CLIENT_ID as string,
+    client_id: import.meta.env[
+      'VITE_GITHUB_CLIENT_ID_' + import.meta.env.MODE
+    ] as string,
     redirect_uri: import.meta.env[
       'VITE_OAUTH_REDIRECT_URL_' + import.meta.env.MODE
     ] as string,

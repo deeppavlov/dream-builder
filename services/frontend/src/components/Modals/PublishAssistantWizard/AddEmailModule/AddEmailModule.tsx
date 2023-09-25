7d69e41532cc748643e8d9d2bd88bc568fad159f
Artem M <artemmarin91@mail.ru>
2023-09-25 11:37:02 +0500

    added submit

--- services/frontend/src/components/Modals/PublishAssistantWizard/AddEmailModule/AddEmailModule.tsx
+++ services/frontend/src/components/Modals/PublishAssistantWizard/AddEmailModule/AddEmailModule.tsx
@@ -1,6 +1,8 @@
+import { useAuth } from 'context'
 import { FC } from 'react'
 import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
+import { authApi } from 'api/axiosConfig'
 import { getValidationSchema } from 'utils/getValidationSchema'
 import { Button } from 'components/Buttons'
 import { Input } from 'components/Inputs'
@@ -16,17 +18,30 @@ interface IProps {
 }
 
 export const AddEmailModule: FC<IProps> = ({ onClose, onContinue }) => {
-  const { reset, control, handleSubmit } = useForm<FormValues>()
+  const { reset, control, handleSubmit, setError } = useForm<FormValues>()
+  const { setUser, user } = useAuth()
   const { t } = useTranslation('translation', {
     keyPrefix: 'modals.add_email_modal',
   })
   const schema = getValidationSchema()
 
-  const onSubmit = ({ email }: FormValues) => {
-    console.log(email)
-    // обновляем юзера
-    reset()
-    onContinue()
+  const onSubmit = async ({ email }: FormValues) => {
+    if (!user) {
+      onClose()
+      return
+    }
+
+    try {
+      const { data } = await authApi.post(
+        `/update_user/${user.id}?user_id=${user.id}&new_email=${email}`
+      )
+      const updatedUser = { ...user, email: data.email }
+      setUser(updatedUser)
+      reset()
+      onContinue()
+    } catch (err) {
+      setError('email', { type: 'manual', message: t('error') })
+    }
   }
 
   return (
@@ -41,11 +56,10 @@ export const AddEmailModule: FC<IProps> = ({ onClose, onContinue }) => {
           <Input
             name='email'
             control={control}
-            label={t('form.label')}
             props={{ placeholder: t('form.placeholder') }}
             rules={{
-              required: schema.global.required,
-              pattern: schema.global.emailPattern,
+              required: schema.globals.required,
+              pattern: schema.globals.emailPattern,
             }}
           />
         </form>

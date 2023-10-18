8469df9d98edaabd69b347666d50430c49e4672b
rafilgalimzanov <rafil.galimzyanov.00@bk.ru>
2023-10-18 10:44:45 +0300

    Removed sending universal skills

--- services/distributions_api/const.py
+++ services/distributions_api/const.py
@@ -6,4 +6,6 @@ TEMPLATE_DIST_PROMPT_BASED = {
 INVISIBLE_COMPONENT_NAMES = [
     "dialogpt",
     "dff_generative_skill",
+    "dff_universal_prompted_skill",
+    "dff_dream_persona_ru_prompted_skill",
 ]

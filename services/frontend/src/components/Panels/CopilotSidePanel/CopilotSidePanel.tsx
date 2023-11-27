1e2a517be6c08fd207008de56de782bb41e8870b
Artem M <artemmarin91@mail.ru>
2023-11-27 19:34:41 +0500

    changes related to the main task

--- services/frontend/src/components/Panels/CopilotSidePanel/CopilotSidePanel.tsx
+++ services/frontend/src/components/Panels/CopilotSidePanel/CopilotSidePanel.tsx
@@ -1,17 +1,21 @@
 import classNames from 'classnames/bind'
-import { useUIOptions } from 'context'
-import React, { useEffect, useRef } from 'react'
+import { useAuth, useUIOptions } from 'context'
+import React, { useEffect, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import { useTranslation } from 'react-i18next'
+import store from 'store2'
 import DeepyHelperIcon from 'assets/icons/deeppavlov_logo_round.svg'
 import { ReactComponent as Renew } from 'assets/icons/renew.svg'
-import { ChatForm, ChatHistory } from 'types/types'
-import { DEEPY_ASSISTANT, TOOLTIP_DELAY } from 'constants/constants'
+import { ChatForm, ChatHistory, TDialogError } from 'types/types'
+import { DEEPY_ASSISTANT, OPEN_AI_LM, TOOLTIP_DELAY } from 'constants/constants'
 import { useDeepyChat } from 'hooks/api'
 import { useChatScroll } from 'hooks/useChatScroll'
+import { useObserver } from 'hooks/useObserver'
 import { consts } from 'utils/consts'
 import { сopyToClipboard } from 'utils/copyToClipboard'
+import { trigger } from 'utils/events'
+import { getLSApiKeyByName } from 'utils/getLSApiKeys'
 import { submitOnEnter } from 'utils/submitOnEnter'
 import { Button } from 'components/Buttons'
 import { Loader, TextLoader } from 'components/Loaders'
@@ -19,12 +23,18 @@ import { BaseToolTip } from 'components/Menus'
 import { SidePanelButtons, SidePanelHeader } from 'components/Panels'
 import { ToastCopySucces } from 'components/UI'
 import s from './CopilotSidePanel.module.scss'
+import DummyErrorPanel from './DummyErrorPanel/DummyErrorPanel'
 
+export type ErrorPanel = {
+  type: 'auth' | 'api-key'
+  msg: string
+}
 export const CopilotSidePanel = () => {
-  const { t } = useTranslation('translation', { keyPrefix: 'sidepanels.deepy' })
+  const { t, i18n } = useTranslation('translation', {
+    keyPrefix: 'sidepanels.deepy',
+  })
   const {
     sendToDeepy,
-    deepyMessage,
     deepyHistory,
     deepySession,
     deepyRemoteHistory,
@@ -33,16 +43,19 @@ export const CopilotSidePanel = () => {
 
   const { handleSubmit, register, reset } = useForm<ChatForm>()
   const { setUIOption } = useUIOptions()
+  const { user } = useAuth()
+
   const cx = classNames.bind(s)
   const chatRef = useRef<HTMLDivElement>(null)
+  const [errorPanel, setErrorPanel] = useState<ErrorPanel | null>(null)
 
   const sendDisabled = sendToDeepy?.isLoading || deepyRemoteHistory?.isLoading
   const renewDisabled = renewDeepySession?.isLoading || sendToDeepy?.isLoading
 
   const historyLoaderActive =
-    deepyRemoteHistory?.isLoading && !deepyRemoteHistory?.error
-  const textLoaderActive =
-    sendToDeepy?.isLoading && !deepyRemoteHistory.isLoading
+    (deepyRemoteHistory?.isFetching && !deepyRemoteHistory?.error) ||
+    renewDeepySession?.isLoading
+  const textLoaderActive = sendToDeepy?.isLoading
 
   // handlers
   const handleMessageClick = (message: string) => {
@@ -70,6 +83,17 @@ export const CopilotSidePanel = () => {
     submitOnEnter(e, !sendToDeepy?.isLoading, handleSubmit(handleSend))
   }
 
+  const handleErrorBtnClick = (type: TDialogError) => {
+    if (type === 'api-key') {
+      trigger('AccessTokensModal', {})
+    }
+    if (type === 'auth') {
+      trigger('SignInModal', {
+        requestModal: { name: 'AccessTokensModal', options: {} },
+      })
+    }
+  }
+
   // hooks
   const dispatchTrigger = (isOpen: boolean) =>
     setUIOption({
@@ -86,7 +110,46 @@ export const CopilotSidePanel = () => {
     return () => dispatchTrigger(false)
   }, [])
 
-  useChatScroll(chatRef, [deepyRemoteHistory?.data, deepyMessage])
+  useObserver(
+    'AccessTokensChanged',
+    ({ detail: { newValue } }) => {
+      if (newValue) {
+        setErrorPanel(null)
+      } else {
+        deepyHistory.at(-1)?.active_skill?.name === 'dummy_skill' &&
+          setErrorPanel({
+            type: 'api-key',
+            msg: t('error.token_error'),
+          })
+      }
+    },
+    [user?.id, deepyHistory]
+  )
+
+  useChatScroll(chatRef, [deepyHistory, errorPanel])
+
+  useEffect(() => {
+    setErrorPanel(null)
+    const openaiApiKey =
+      getLSApiKeyByName(user?.id!, OPEN_AI_LM, true) || undefined
+    const localSession = store('deepySession')
+    if (
+      localSession?.dummy ||
+      (deepyHistory.at(-1)?.active_skill?.name === 'dummy_skill' &&
+        !openaiApiKey)
+    ) {
+      if (!user) {
+        setErrorPanel({
+          type: 'auth',
+          msg: t('error.auth_error'),
+        })
+      } else
+        setErrorPanel({
+          type: 'api-key',
+          msg: t('error.token_error'),
+        })
+    }
+  }, [deepyHistory, i18n.language])
 
   return (
     <div className={s.container}>
@@ -97,6 +160,7 @@ export const CopilotSidePanel = () => {
             Deepy
           </span>
         </SidePanelHeader>
+
         <div className={s.chat} ref={chatRef}>
           {historyLoaderActive ? (
             <div className={s.loaderWrapper}>
@@ -134,42 +198,49 @@ export const CopilotSidePanel = () => {
             </div>
           )}
         </div>
-
-        <form onSubmit={handleSubmit(handleSend)}>
-          <textarea
-            spellCheck='false'
-            onKeyDown={handleKeyDown}
-            className={s.textarea}
-            placeholder={t('message_field.placeholder')}
-            {...register('message', { required: true })}
+        {errorPanel ? (
+          <DummyErrorPanel
+            errorPanel={errorPanel}
+            handleErrorBtnClick={handleErrorBtnClick}
+            handleRenewClick={handleRenewClick}
           />
-          <input type='submit' hidden />
-          <SidePanelButtons>
-            <Button
-              theme='secondary'
-              props={{
-                disabled: renewDisabled,
-                onClick: handleRenewClick,
-              }}
-            >
-              <BaseToolTip
-                delayShow={TOOLTIP_DELAY}
-                id='renew'
-                content={t('tooltips.dialog_renew')}
-              />
-              <Renew data-tooltip-id='renew' />
-            </Button>
-            <Button
-              theme='primary'
-              props={{
-                disabled: sendDisabled,
-                type: 'submit',
-              }}
-            >
-              {t('btns.send')}
-            </Button>
-          </SidePanelButtons>
-        </form>
+        ) : (
+          <form onSubmit={handleSubmit(handleSend)}>
+            <textarea
+              spellCheck='false'
+              onKeyDown={handleKeyDown}
+              className={s.textarea}
+              placeholder={t('message_field.placeholder')}
+              {...register('message', { required: true })}
+            />
+            <input type='submit' hidden />
+            <SidePanelButtons>
+              <Button
+                theme='secondary'
+                props={{
+                  disabled: renewDisabled,
+                  onClick: handleRenewClick,
+                }}
+              >
+                <BaseToolTip
+                  delayShow={TOOLTIP_DELAY}
+                  id='renew'
+                  content={t('tooltips.dialog_renew')}
+                />
+                <Renew data-tooltip-id='renew' />
+              </Button>
+              <Button
+                theme='primary'
+                props={{
+                  disabled: sendDisabled,
+                  type: 'submit',
+                }}
+              >
+                {t('btns.send')}
+              </Button>
+            </SidePanelButtons>
+          </form>
+        )}
       </div>
     </div>
   )

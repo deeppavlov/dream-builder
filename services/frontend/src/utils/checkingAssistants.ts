809f12c2dcb2a0734993a777813a00bb830e850c
saga6569 <aristov6569@gmail.com>
2023-12-06 09:43:25 +0300

    updated

--- services/frontend/src/utils/checkingAssistants.ts
+++ services/frontend/src/utils/checkingAssistants.ts
@@ -1,28 +1,71 @@
-import { franc, francAll } from 'https://esm.sh/franc@6';
-import i18n from 'i18n';
-import LanguageDetect from 'languagedetect';
-import { UseQueryResult } from 'react-query';
-import { ICollectionError, ISkill, TComponents } from 'types/types';
+import { franc, francAll } from 'https://esm.sh/franc@6'
+import i18n from 'i18n'
+import LanguageDetect from 'languagedetect'
+import { UseQueryResult } from 'react-query'
+import { ICollectionError, ISkill, TComponents } from 'types/types'
 
-
-//(?:\b\w+\b|\[.*?\]|[А-я_]+)
-const arrInitPromptBlock = [
-  'Act as [YOUR INPUT].',
-  'YOUR PERSONALITY: \nYour name is [YOUR INPUT]. Your interests are: [YOUR INPUT].',
-  'TASK: \nour task is to do [YOUR INPUT].',
-  'Use [YOUR INPUT] voice and tone.',
-  'Reply in [YOUR INPUT] language.',
-  'The target audience is [YOUR INPUT].',
-  'I want you to reply in [YOUR INPUT] format.',
-  'Limit your replies to [YOUR INPUT] words.',
-  'Your ultimate goal is to persuade human to do [YOUR INPUT].',
-  'CONTEXT ABOUT HUMAN: \n"""[YOUR INPUT]""".',
-  'INSTRUCTION: \n"""[YOUR INPUT]""".',
-  'EXAMPLE:\n"""[YOUR INPUT]""".',
-  "Don't reply to following topics: [YOUR INPUT].",
-  'Reply to human from [YOUR INPUT] point of view.',
-  'Я хочу, чтобы вы выступили в роли [ВАШ ВВОД].',
-  'ВАША ЛИЧНОСТЬ: Ваше имя: [ВАШ ВВОД]. Ваши интересы: [ВАШ ВВОД].',
+const arrInitPromptBlock2 = [
+  { 'Act as [YOUR INPUT].': /[Aa]ct as (?:\b\w+\b|\[.*?\]|[А-я_]+)/gm },
+  {
+    'YOUR PERSONALITY: \nYour name is [YOUR INPUT]. Your interests are: [YOUR INPUT].':
+      /YOUR PERSONALITY: [Yy]our name is (?:\b\w+\b|\[.*?\]|[А-я_]+). [Yy]our interests are: (?:\b\w+\b|\[.*?\]|[А-я_]+)/gm,
+  },
+  {
+    'TASK: \nour task is to do [YOUR INPUT].':
+      /TASK: our task is to do (?:\b\w+\b|\[.*?\]|[А-я_]+)/gm,
+  },
+  {
+    'Use [YOUR INPUT] voice and tone.':
+      /[Uu]se (?:\b\w+\b|\[.*?\]|[А-я_]+) voice and tone/gm,
+  },
+  {
+    'Reply in [YOUR INPUT] language.':
+      /[Rr]eply in (?:\b\w+\b|\[.*?\]|[А-я_]+) language/gm,
+  },
+  {
+    'The target audience is [YOUR INPUT].':
+      /[Tt]he target audience is (?:\b\w+\b|\[.*?\]|[А-я_]+)./gm,
+  },
+  {
+    'I want you to reply in [YOUR INPUT] format.':
+      /[Ii] want you to reply in (?:\b\w+\b|\[.*?\]|[А-я_]+) format/gm,
+  },
+  {
+    'Limit your replies to [YOUR INPUT] words.':
+      /[Ll]imit your replies to (?:\b\w+\b|\[.*?\]|[А-я_]+) words./gm,
+  },
+  {
+    'Your ultimate goal is to persuade human to do [YOUR INPUT].':
+      /[Yy]our ultimate goal is to persuade human to do (?:\b\w+\b|\[.*?\]|[А-я_]+)/gm,
+  },
+  {
+    'CONTEXT ABOUT HUMAN: \n"""[YOUR INPUT]""".':
+      /CONTEXT ABOUT HUMAN: """(?:\b\w+\b|\[.*?\]|[А-я_]+)"""/gm,
+  },
+  {
+    'INSTRUCTION: \n"""[YOUR INPUT]""".':
+      /INSTRUCTION: """(?:\b\w+\b|\[.*?\]|[А-я_]+)"""/gm,
+  },
+  {
+    'EXAMPLE:\n"""[YOUR INPUT]""".':
+      /EXAMPLE:"""(?:\b\w+\b|\[.*?\]|[А-я_]+)"""/gm,
+  },
+  {
+    "Don't reply to following topics: [YOUR INPUT].":
+      /[Dd]on't reply to following topics: (?:\b\w+\b|\[.*?\]|[А-я_]+)/gm,
+  },
+  {
+    'Reply to human from [YOUR INPUT] point of view.':
+      /[Rr]eply to human from (?:\b\w+\b|\[.*?\]|[А-я_]+) point of view/gm,
+  },
+  {
+    'Я хочу, чтобы вы выступили в роли [ВАШ ВВОД].':
+      /[Яя] хочу, чтобы вы выступили в роли (?:\b\w+\b|\[.*?\]|[А-я_]+)/gm,
+  },
+  {
+    'ВАША ЛИЧНОСТЬ: Ваше имя: [ВАШ ВВОД]. Ваши интересы: [ВАШ ВВОД].':
+      /ВАША ЛИЧНОСТЬ: Ваше имя: (?:\b\w+\b|\[.*?\]|[А-я_]+). Ваши интересы: (?:\b\w+\b|\[.*?\]|[А-я_]+)/gm,
+  },
 ]
 
 const InputPrompt = (skill: ISkill, acc: ICollectionError) => {
@@ -36,9 +79,7 @@ const InputPrompt = (skill: ISkill, acc: ICollectionError) => {
     ''
   )
 
-  // console.log(str)
-
-  if (true) {
+  if (str.length !== 0) {
     const newError = {
       status: 'error',
       massage: `${i18n.t('error_massage.prompt.input')} ${str}`,
@@ -52,13 +93,36 @@ const lengthPrompt = (skill: ISkill, acc: ICollectionError) => {
     return
   }
 
-  const result = skill.prompt.length < 1
-  if (result) {
+  if (skill.prompt.length < 3) {
     const newWarning = {
-      status: 'error',
+      status: 'warning',
       massage: i18n.t('error_massage.prompt.length'),
     }
     acc.warning = [...acc.warning, newWarning]
+  }
+}
+
+const typePrompt = (skill: ISkill, acc: ICollectionError) => {
+  if (skill.prompt === undefined || skill.prompt === null) {
+    return
+  }
+
+  const arr = ['+', '-', '=', '*', '/', '%', '(', ')']
+
+  const result = skill.prompt.replace(/(?:\b[a-zA-Z]+\b|\[.*?\]|[А-я_]+)/gi, '')
+
+  const str = skill.prompt.match(/(?:\b[a-zA-Z]+\b|\[.*?\]|[А-я_]+)/gi)
+
+  const arrIsIncludes = arr.map(el => result.includes(el))
+
+  if (arrIsIncludes.includes(true) && str === null) {
+    const newWarning = {
+      status: 'warning',
+      massage:
+        'Возможно вы используете арифметические выражение в своей инструкции',
+    }
+    acc.warning = [...acc.warning, newWarning]
+    return
   }
 }
 
@@ -70,66 +134,62 @@ const languagePrompt = (skill: ISkill, acc: ICollectionError) => {
   const languageArrSkill: string[] | undefined =
     skill.lm_service?.languages?.map(el => el.value)
 
+  const result = franc(skill.prompt, {
+    minLength: 3,
+    only: ['rus', 'eng'],
+  }).slice(0, 2)
 
-
- const r = franc(skill.prompt, {only: ['rus', 'eng']})
-
- console.log(r)
-
-  if (!languageArrSkill) {
+  if (result === 'un') {
+    const newError = {
+      status: 'error',
+      massage: `мы не смогли определить язык вашего промта`,
+    }
+    acc.error = [...acc.error, newError]
     return
   }
 
-  const promptLanguage = 'заглушка'
-
-  if (true) {
-    const newWarning = {
-      status: 'error',
-      massage: `${i18n.t('error_massage.prompt.language')} ${promptLanguage}`,
-    }
-    acc.warning = [...acc.warning, newWarning]
+  if (languageArrSkill?.includes(result)) {
+    return
   }
+
+  const newWarning = {
+    status: 'error',
+    massage: `${i18n.t('error_massage.prompt.language')} ${result}`,
+  }
+  acc.warning = [...acc.warning, newWarning]
 }
 
 const promptBlocks = (skill: ISkill, acc: ICollectionError) => {
-  if (skill.prompt === undefined) {
-    return
-  }
-
-  if (skill.lm_service?.prompt_blocks?.length === 0) {
-    return
-  }
-
-  let accString = ''
+  const promtText =
+    skill.prompt === undefined ? '' : skill.prompt.replace(/\s+/g, ' ').trim()
 
   const promptBlocks = skill.lm_service?.prompt_blocks?.map(el => el.template)
 
-  const invalidBlocks = arrInitPromptBlock.filter(
-    el => !promptBlocks?.includes(el)
+  const invalidBlocks = arrInitPromptBlock2.filter(
+    el => !promptBlocks?.includes(Object.keys(el)[0])
   )
 
-  const arrIsinvalidBlocks = invalidBlocks?.map(el => {
-    const regString = '(?:\\b\\w+\\b|\\[.*?\\]|[А-я_]+)'
+  console.log()
 
-    const strReg = el
-      .replaceAll('[YOUR INPUT]', regString)
-      .replaceAll('[ВАШ ВВОД]', regString)
+  const arrIsinvalidBlocks = invalidBlocks?.reduce((acc: string[], el: any) => {
+    const key = Object.keys(el)[0]
+    const reg = el[key]
 
-    const reg = new RegExp(strReg, 'g')
+    const arrString = promtText.match(reg)
 
-    const result = reg.test(skill.prompt)
-
-    if (result) {
-      accString += el
+    if (arrString === null) {
+      return acc
     }
 
-    return result
-  })
+    return [...acc, ...arrString]
+  }, [])
 
-  if (arrIsinvalidBlocks.includes(true)) {
+  if (arrIsinvalidBlocks.length !== 0) {
     const newWarning = {
       status: 'warning',
-      massage: `Возожно вы используете блок который не поддерживает этот скилл ${accString}`,
+      massage: `Возожно вы используете блок который не поддерживает этот скилл \n${arrIsinvalidBlocks.join(
+        ',\n '
+      )}`,
     }
     acc.warning = [...acc.warning, newWarning]
   }
@@ -145,6 +205,7 @@ export const examination = (data: ISkill) => {
   lengthPrompt(data, acc)
   languagePrompt(data, acc)
   promptBlocks(data, acc)
+  typePrompt(data, acc)
   return acc
 }
 

8a8299ee6aa86c67569c755e701b676beab3a9b2
Artem M <artemmarin91@mail.ru>
2024-03-29 22:31:32 +0500

    fixed check if user is an admin

--- services/frontend/src/router/AdminRoute.tsx
+++ services/frontend/src/router/AdminRoute.tsx
@@ -1,15 +1,17 @@
-import { Navigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import store from 'store2'
 import { UserInterface } from 'types/types'
 
 export const AdminRoute = ({ children }: any) => {
-  // If not admin redirect to main page
-
-  const q = import.meta.env?.VITE_ADMINS?.split(' ')
   const user: UserInterface = store('user')
-  const isAdmin = q.includes(user?.email)
+  const isAdmin = user?.role.id === 2 || user?.role.id === 3
+  const n = useNavigate()
 
-  if (!localStorage.getItem('user') || !isAdmin) return <Navigate to='/' />
+  useEffect(() => {
+    // If not admin redirect to main page
+    !isAdmin && n('/')
+  }, [user?.role.id])
 
   // If admin return Route
   return children

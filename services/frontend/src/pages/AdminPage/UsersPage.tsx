e4f87e6f1c392e1a8f6b28eebbf1d12e6eec4dba
Artem M <artemmarin91@mail.ru>
2024-04-15 16:58:35 +0500

    admin page refactoring

---
+++ services/frontend/src/pages/AdminPage/UsersPage.tsx
@@ -0,0 +1,12 @@
+import { UserList } from 'components/Tables'
+import { Main } from 'components/UI'
+
+const UsersPage = () => {
+  return (
+    <Main sidebar>
+      <UserList />
+    </Main>
+  )
+}
+
+export default UsersPage

2c911f1cb7903438500b31c84cd19cb79b2e3d39
rafilgalimzanov <rafil.galimzyanov.00@bk.ru>
2024-05-03 10:41:24 +0300

    Added status and type fields, new APIs

--- services/feedback_api/feedback.py
+++ services/feedback_api/feedback.py
@@ -1,10 +1,13 @@
+from typing import List, Optional
+
 from fastapi import APIRouter, Depends, Header, status, HTTPException
 from sqlalchemy.orm import Session
 
 from database.core import init_db
 from apiconfig.config import settings
-from database.models.feedback.crud import add_feedback
-from services.feedback_api.models import FeedbackCreate, FeedbackResponse
+from database.models.feedback.crud import add_feedback, get_feedback, get_all_feedback_statuses, get_all_feedback_types
+from services.feedback_api.auth import get_admin_user
+from services.feedback_api.models import FeedbackCreate, UserRead
 
 router = APIRouter(prefix="/feedback")
 SessionLocal = init_db(settings.db.user, settings.db.password, settings.db.host, settings.db.port, settings.db.name)
@@ -25,3 +28,33 @@ async def create_feedback(
     """
     """
     return add_feedback(db, **payload.dict())
+
+
+@router.get("")
+async def get_feedbacks(
+        user: UserRead = Depends(get_admin_user),
+        type_id: Optional[int] = None,
+        status_id: Optional[int] = None,
+        db: Session = Depends(get_db)
+):
+    """
+    """
+    return get_feedback(db, type_id, status_id)
+
+
+@router.get("/status/all")
+async def get_feedbacks(
+        db: Session = Depends(get_db)
+):
+    """
+    """
+    return get_all_feedback_statuses(db)
+
+
+@router.get("/type/all")
+async def get_feedbacks(
+        db: Session = Depends(get_db)
+):
+    """
+    """
+    return get_all_feedback_types(db)
